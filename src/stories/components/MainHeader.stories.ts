9878946d5e9283d77a1667ea70f62dc13919cd8a
loevray <rei980213@gmail.com>
2023-12-28 19:01:01 +0900

    :sparkles: MainHeader컴포넌트 레이아웃과 스토리 작성완료

---
+++ src/stories/components/MainHeader.stories.ts
@@ -0,0 +1,24 @@
+import MainHeader from '@/components/Test/MainHeader';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof MainHeader> = {
+  component: MainHeader,
+  argTypes: {
+    width: {
+      control: 'number',
+    },
+    height: {
+      control: 'number',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof MainHeader>;
+
+export const Deafult: Story = {
+  args: {
+    width: 480,
+    height: 80,
+  },
+};
