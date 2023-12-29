2d464e2a601f04fe29bc20ad323e99ce49976a44
seunghyun <tmdgus7820@naver.com>
2023-12-30 01:08:27 +0900

    ⚡️ #8 useInput hook 코드 개선 (dom 접근을 위한 ref 추가)

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,7 +1,8 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useRef } from 'react';
 
 const useInput = (initialValue: string) => {
   const [value, setValue] = useState(initialValue);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const onChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     setValue(e.currentTarget.value);
@@ -11,7 +12,7 @@ const useInput = (initialValue: string) => {
     setValue(initialValue);
   }, [initialValue]);
 
-  return { value, onChange, reset };
+  return { value, onChange, inputRef, reset };
 };
 
 export default useInput;
