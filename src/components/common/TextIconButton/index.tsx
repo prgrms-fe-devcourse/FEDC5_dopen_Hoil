d06affe6820128809060859de44a34334d0c3987
loevray <rei980213@gmail.com>
2023-12-31 02:26:40 +0900

    :sparkles: Footer컴포넌트 내부 icon과 text를 TextIconButton컴포넌트로 추상화 및 변경
    - 텍스트 크기 1rem => 1.2rem으로 변경

---
+++ src/components/common/TextIconButton/index.tsx
@@ -0,0 +1,25 @@
+import { Flex, Icon, IconButton, Text } from '@chakra-ui/react';
+import { IconType } from 'react-icons';
+
+interface TextIconButtonProps {
+  TheIcon: IconType;
+  textContent: string;
+}
+
+const TextIconButton = ({ TheIcon, textContent }: TextIconButtonProps) => {
+  return (
+    <Flex flexDir="column" align="center" cursor="pointer" role="group">
+      <IconButton
+        aria-label="home"
+        icon={<Icon as={TheIcon} boxSize="icon" color="black" />}
+        bg="white"
+        _groupHover={{ background: 'gray.450' }}
+      />
+      <Text fontSize="sm" fontWeight="800" textAlign="center" color="black">
+        {textContent}
+      </Text>
+    </Flex>
+  );
+};
+
+export default TextIconButton;
---
+++ src/stories/components/TextIconButton.stories.ts
@@ -0,0 +1,11 @@
+import TextIconButton from '@/components/common/TextIconButton';
+import { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<typeof TextIconButton> = {
+  component: TextIconButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof TextIconButton>;
+
+export const Deafult: Story = {};
