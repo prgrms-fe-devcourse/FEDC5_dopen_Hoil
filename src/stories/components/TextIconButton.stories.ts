b9b2ad78e63e91424cc84fc35bd7d7c73536387e
loevray <rei980213@gmail.com>
2023-12-31 02:36:58 +0900

    :sparkles: TextIconButton storybook에 견본 추가 - default, 홈, 랭킹

--- src/stories/components/TextIconButton.stories.ts
+++ src/stories/components/TextIconButton.stories.ts
@@ -1,11 +1,35 @@
 import TextIconButton from '@/components/common/TextIconButton';
 import { Meta, StoryObj } from '@storybook/react';
+import { MdEmojiEvents, MdHome } from 'react-icons/md';
 
 const meta: Meta<typeof TextIconButton> = {
   component: TextIconButton,
+  argTypes: {
+    textContent: {
+      control: 'text',
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof TextIconButton>;
 
-export const Deafult: Story = {};
+export const Deafult: Story = {
+  args: {
+    textContent: '테스트',
+  },
+};
+
+export const Home: Story = {
+  args: {
+    TheIcon: MdHome,
+    textContent: '홈',
+  },
+};
+
+export const Rank: Story = {
+  args: {
+    TheIcon: MdEmojiEvents,
+    textContent: '랭킹',
+  },
+};
