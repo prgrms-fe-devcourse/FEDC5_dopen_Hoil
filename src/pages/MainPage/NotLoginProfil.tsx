567b14b70d124cdaf6546d643a70acd799c413ba
JaeHyunGround <gothddlek@naver.com>
2023-12-31 15:18:27 +0900

    ✨ MainPage 프로필 영역 작업 완료
    - MainPage의 프로필 영역 작업을 마무리 하였습니다.
    
    TODO
    - D-Day, 잔디, 게시판 영역 작업 필요

---
+++ src/pages/MainPage/NotLoginProfil.tsx
@@ -0,0 +1,29 @@
+import { Avatar, Flex, Text } from '@chakra-ui/react';
+
+const Profil = () => {
+  return (
+    <Flex alignItems="center">
+      <Avatar size="2xl" />
+      <Flex direction="column" marginLeft="20px">
+        <Text
+          width="fit-content"
+          fontSize="3xl"
+          fontWeight="bold"
+          color="pink.300"
+          borderBottom="2px"
+          borderColor="transparent"
+          transition="border-bottom 0.5s"
+          _hover={{ cursor: 'pointer', borderColor: 'pink.300' }}
+        >
+          {/* TODO : 로그인 버튼 클릭 시 로그인 페이지로 넘어가도록 처리 필요. */}
+          로그인
+        </Text>
+        <Text fontStyle="oblique" fontSize="md" cursor="default">
+          도펜에 가입한 사람이 가장 많은 시간을 갖는다.
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Profil;
