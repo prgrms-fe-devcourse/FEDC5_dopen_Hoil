5a2d987b9246114bbfd4dacc586589d053320e60
이종혁 <whdgur57117@gmail.com>
2024-01-01 02:55:13 +0900

    ✨ axios 추상화 함수 추가 axios.get,post 요청 추상화 작업

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-
+import { AxiosRequestConfig, AxiosResponse } from 'axios';
 const API_ENDPOINT = `${import.meta.env.VITE_APP_URL}:${
   import.meta.env.VITE_APP_PORT
 }`;
@@ -10,3 +10,24 @@ export const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+export const getRequest = async <T>(
+  url: string,
+  config?: AxiosRequestConfig,
+): Promise<T> => {
+  const { data } = await axiosInstance.get<T, AxiosResponse<T>>(url, config);
+  return data;
+};
+
+export const postRequest = async <T, U>(
+  url: string,
+  body?: U,
+  config?: AxiosRequestConfig,
+): Promise<T> => {
+  const { data } = await axiosInstance.post<T, AxiosResponse<T>, U>(
+    url,
+    body,
+    config,
+  );
+  return data;
+};
