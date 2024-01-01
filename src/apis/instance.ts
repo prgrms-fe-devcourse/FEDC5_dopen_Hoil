8c29d208021e1153718aafd9c7411fb021971ece
이종혁 <whdgur57117@gmail.com>
2024-01-01 12:53:54 +0900

    ✨ API 추상화 함수 추가 - axios.delete , put 메서드 추상화 함수 추가

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
 const API_ENDPOINT = `${import.meta.env.VITE_APP_URL}:${
   import.meta.env.VITE_APP_PORT
 }`;
@@ -10,7 +10,7 @@ export const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
-//TODO: 함수명 수정 필요할 것 같음
+
 export const getRequest = async <T>(
   url: string,
   config?: AxiosRequestConfig,
@@ -29,5 +29,30 @@ export const postRequest = async <T, U>(
     body,
     config,
   );
+  return data;
+};
+
+export const putRequest = async <T, U>(
+  url: string,
+  body?: U,
+  config?: AxiosRequestConfig,
+): Promise<T> => {
+  const { data } = await axiosInstance.put<T, AxiosResponse<T>, U>(
+    url,
+    body,
+    config,
+  );
+  return data;
+};
+//TODO: 추후 테스트 필요(현재는 데이터가 없어서 테스트 힘듦)
+export const deleteRequest = async <T, U>(
+  url: string,
+  body?: U,
+  config?: AxiosRequestConfig,
+): Promise<T> => {
+  const { data } = await axiosInstance.delete<T, AxiosResponse<T>, U>(url, {
+    data: { ...body },
+    ...config,
+  });
   return data;
 };
