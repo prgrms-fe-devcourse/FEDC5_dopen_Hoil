4d3c5840bd5041c78c51b9bcd492388a585b3a65
JaeHyunGround <gothddlek@naver.com>
2024-01-01 10:38:55 +0900

    🐛 width 상수명 변경

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,9 +1,9 @@
 import MainHeader from '@/components/MainHeader';
-import NotLoginProfil from '@/pages/MainPage/NotLoginProfil';
-import { WIDTH } from '@/constants/style';
+import NotLoginProfil from '@/pages/MainPage/NotLoginProfile';
+import { defaultWidth } from '@/constants/style';
 import { Box, Flex } from '@chakra-ui/react';
 import { useState } from 'react';
-import LoginedProfil from './LoginedProfil';
+import LoginedProfil from './LoginProfile';
 
 const MainPage = () => {
   const [isLogin, setIsLoigin] = useState(false); // 로그인 유무를 구분하기 위한 임시 값입니다.
@@ -11,18 +11,18 @@ const MainPage = () => {
     <>
       <Flex
         position="relative"
-        w={WIDTH}
+        w={defaultWidth}
         height="100vh"
         margin="0 auto"
         direction="column"
       >
         <MainHeader />
-        <Flex w={WIDTH} p="20px 10px 90px 10px" overflowY="auto">
+        <Flex w={defaultWidth} p="20px 10px 90px 10px" overflowY="auto">
           {isLogin ? <LoginedProfil /> : <NotLoginProfil />}
         </Flex>
         {/* 하단 네비게이터 위치 */}
         <Box
-          w={WIDTH}
+          w={defaultWidth}
           position="absolute"
           bottom="0"
           height="80px"
