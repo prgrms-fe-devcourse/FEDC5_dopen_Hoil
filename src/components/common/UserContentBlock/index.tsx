37fe0dbcda8ea51d86456b75d4f8891090f9fa89
loevray <rei980213@gmail.com>
2024-01-01 22:59:11 +0900

    :lipstick: UserContentBlock 프롭스 할당 및 레이아웃 완성

--- src/components/common/UserContentBlock/index.tsx
+++ src/components/common/UserContentBlock/index.tsx
@@ -1,12 +1,52 @@
 import { Avatar, Flex, Text, VStack } from '@chakra-ui/react';
 
-const UserContentBlock = () => {
+interface UserContentBlock {
+  width?: string | number;
+  height?: string | number;
+  userImage: string;
+  username: string;
+  content: string | number;
+  usernameFontSize?: string | number;
+  contentFontSize?: string | number;
+}
+
+const UserContentBlock = ({
+  width,
+  height,
+  userImage = '',
+  content,
+  username = '테스트용',
+  usernameFontSize = '1.4rem',
+  contentFontSize = '1.2rem',
+}: UserContentBlock) => {
   return (
-    <Flex>
-      <Avatar name="Kent Dodds"></Avatar>
-      <VStack>
-        <Text>닉네임</Text>
-        <Text>내용</Text>
+    /* width나 heigth값이 존재할시만 적용. */
+    <Flex
+      w={width && typeof width === 'string' ? width : `${width}px`}
+      h={height && typeof height === 'string' ? height : `${height}px`}
+      align="center"
+      gap="17px"
+    >
+      <Avatar src={userImage} boxSize="50px" />
+      <VStack align="left">
+        <Text
+          fontSize={
+            typeof usernameFontSize === 'string'
+              ? usernameFontSize
+              : `${usernameFontSize}px`
+          }
+        >
+          {username}
+        </Text>
+        <Text
+          fontSize={
+            typeof contentFontSize === 'string'
+              ? contentFontSize
+              : `${contentFontSize}px`
+          }
+        >
+          {content}
+        </Text>
       </VStack>
     </Flex>
   );
