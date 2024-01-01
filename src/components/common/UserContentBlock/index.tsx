b5e49d49bafda494cc4dbf2646c88428d57a6113
loevray <rei980213@gmail.com>
2024-01-01 23:15:01 +0900

    :sparkles: UserContentBlock컴포넌트 navigate추가 및 아바타 클릭이벤트 받아올수 있게 추가 - 둘 다 props로 받아옵니다

--- src/components/common/UserContentBlock/index.tsx
+++ src/components/common/UserContentBlock/index.tsx
@@ -1,4 +1,5 @@
 import { Avatar, Flex, Text, VStack } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 interface UserContentBlock {
   width?: string | number;
@@ -8,6 +9,8 @@ interface UserContentBlock {
   content: string | number;
   usernameFontSize?: string | number;
   contentFontSize?: string | number;
+  href?: string;
+  onImageClick?: () => void;
 }
 
 const UserContentBlock = ({
@@ -18,16 +21,29 @@ const UserContentBlock = ({
   username = '테스트용',
   usernameFontSize = '1.4rem',
   contentFontSize = '1.2rem',
+  href,
+  onImageClick,
 }: UserContentBlock) => {
+  const navigate = useNavigate();
+  const onNavigate = () => navigate(`${href}`);
   return (
-    /* width나 heigth값이 존재할시만 적용. */
+    /* 
+    1. width나 heigth값이 존재할시만 적용. 
+    2. href가 있을시만 네비게이팅. 만약 이미지 클릭이 있다면...고려해봐야할 사항
+    */
     <Flex
       w={width && typeof width === 'string' ? width : `${width}px`}
       h={height && typeof height === 'string' ? height : `${height}px`}
       align="center"
       gap="17px"
+      onClick={() => href && onNavigate()}
+      cursor="pointer"
     >
-      <Avatar src={userImage} boxSize="50px" />
+      <Avatar
+        src={userImage}
+        boxSize="50px"
+        onClick={() => onImageClick && onImageClick()}
+      />
       <VStack align="left">
         <Text
           fontSize={
