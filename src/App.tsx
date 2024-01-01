aacf35cbcedb1a314d328fcb08bc35e4fbdf1265
seunghyun <tmdgus7820@naver.com>
2024-01-01 21:41:36 +0900

    🔧 LocalStorage Utils 함수 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
+import SignUp from '@/components/member/SignUp';
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </>
   );
