c1a08b0e4a4762f8ecc9ba49fdfe3b0c73b4af47
이종혁 <whdgur57117@gmail.com>
2024-01-02 11:17:17 +0900

    ✨ axios interceptor 기능 추가 - interceptors를 통한 토큰 처리

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,4 +1,4 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 
 const API_ENDPOINT = `${import.meta.env.VITE_APP_URL}:${
   import.meta.env.VITE_APP_PORT
@@ -11,11 +11,24 @@ export const axiosInstance = axios.create({
   },
 });
 
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token && config.headers) {
+    config.headers.Authorization = `bearer ${token}`;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  (err) => Promise.reject(err),
+);
+
 export const getRequest = async <T>(
   url: string,
   config?: AxiosRequestConfig,
 ): Promise<T> => {
-  const { data } = await axiosInstance.get<T, AxiosResponse<T>>(url, config);
+  const { data } = await axiosInstance.get<T>(url, config);
   return data;
 };
 
@@ -24,11 +37,7 @@ export const postRequest = async <T, U>(
   body?: U,
   config?: AxiosRequestConfig,
 ): Promise<T> => {
-  const { data } = await axiosInstance.post<T, AxiosResponse<T>, U>(
-    url,
-    body,
-    config,
-  );
+  const { data } = await axiosInstance.post<T>(url, body, config);
   return data;
 };
 
@@ -37,11 +46,7 @@ export const putRequest = async <T, U>(
   body?: U,
   config?: AxiosRequestConfig,
 ): Promise<T> => {
-  const { data } = await axiosInstance.put<T, AxiosResponse<T>, U>(
-    url,
-    body,
-    config,
-  );
+  const { data } = await axiosInstance.put<T>(url, body, config);
   return data;
 };
 //TODO: 추후 테스트 필요(현재는 데이터가 없어서 테스트 힘듦)
@@ -50,7 +55,7 @@ export const deleteRequest = async <T, U>(
   body?: U,
   config?: AxiosRequestConfig,
 ): Promise<T> => {
-  const { data } = await axiosInstance.delete<T, AxiosResponse<T>, U>(url, {
+  const { data } = await axiosInstance.delete<T>(url, {
     data: { ...body },
     ...config,
   });
