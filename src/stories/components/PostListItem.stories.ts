0b70f9a602e527506d59ece4805a726339432af4
loevray <rei980213@gmail.com>
2024-01-02 16:45:55 +0900

    :lipstick: PostListItem 레이아웃 완료

--- src/stories/components/PostListItem.stories.ts
+++ src/stories/components/PostListItem.stories.ts
@@ -7,6 +7,9 @@ const meta: Meta<typeof PostListItem> = {
     title: {
       control: 'text',
     },
+    body: {
+      control: 'text',
+    },
     username: {
       control: 'text',
     },
@@ -27,7 +30,8 @@ type Story = StoryObj<typeof PostListItem>;
 
 export const Deafult: Story = {
   args: {
-    title: '제목입니다',
+    title: '제목입니다제목입니다제목입니다제목입니다제목입니다',
+    body: '내용이에요내용이에요내용이에요내용이에요내용이에요내용이에',
     username: '올챙이',
     timeAgo: '2일전',
     likeCount: 10,
