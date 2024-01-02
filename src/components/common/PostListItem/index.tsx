006265636498744cf75af9856ff64e1f32ef954b
loevray <rei980213@gmail.com>
2024-01-02 15:40:55 +0900

    :art: TextIconButton 아이콘 컬러, 텍스트 컬러, 사이즈, 굵기 정할 수 있게 props할당 및 storybook수정

--- src/components/common/PostListItem/index.tsx
+++ src/components/common/PostListItem/index.tsx
@@ -1,4 +1,7 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { DEFAULT_PAGE_PADDING, DEFAULT_WIDTH } from '@/constants/style';
+import { Flex, Text, VStack } from '@chakra-ui/react';
+import TextIconButton from '../TextIconButton';
+import { MdFavorite } from 'react-icons/md';
 
 interface PostListItemProps {
   title: string;
@@ -16,13 +19,33 @@ const PostListItem = ({
   commentCount,
 }: PostListItemProps) => {
   return (
-    <Flex>
-      <Box>
-        {title} {username} {timeAgo}
-      </Box>
-      <Box>
-        {likeCount} {commentCount}
-      </Box>
+    <Flex
+      w={DEFAULT_WIDTH}
+      h="67px"
+      pl={DEFAULT_PAGE_PADDING}
+      pr={DEFAULT_PAGE_PADDING}
+      border="1px solid black"
+      align="center"
+      justify="space-between"
+    >
+      <VStack spacing="0" align="left">
+        <Text color="black" fontSize="1.4rem" fontWeight="semibold">
+          {title}
+        </Text>
+        <Text color="gray.800" fontSize="1.2rem" fontWeight="medium">
+          {username}
+        </Text>
+        <Text color="gray.600" fontSize="1.2rem">
+          {timeAgo}
+        </Text>
+      </VStack>
+      <VStack spacing="0">
+        <TextIconButton
+          TheIcon={MdFavorite}
+          textContent={likeCount.toString()}
+        />
+        {commentCount}
+      </VStack>
     </Flex>
   );
 };
--- src/stories/components/PostListItem.stories.ts
+++ src/stories/components/PostListItem.stories.ts
@@ -3,12 +3,34 @@ import { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof PostListItem> = {
   component: PostListItem,
-  argTypes: {},
+  argTypes: {
+    title: {
+      control: 'text',
+    },
+    username: {
+      control: 'text',
+    },
+    timeAgo: {
+      control: 'text',
+    },
+    likeCount: {
+      control: 'number',
+    },
+    commentCount: {
+      control: 'number',
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof PostListItem>;
 
 export const Deafult: Story = {
-  args: {},
+  args: {
+    title: '제목입니다',
+    username: '올챙이',
+    timeAgo: '2일전',
+    likeCount: 10,
+    commentCount: 20,
+  },
 };
