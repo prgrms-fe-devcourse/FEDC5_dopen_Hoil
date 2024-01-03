c85d28152300926e73ba962ee24e288968c9f36a
JaeHyunGround <gothddlek@naver.com>
2024-01-03 10:42:23 +0900

    🎨 Dday 컴포넌트 분리

--- src/pages/MainPage/Dday.tsx
+++ src/pages/MainPage/Dday.tsx
@@ -1,4 +1,6 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
+import LoginDday from '@/pages/MainPage/LoginDday';
+import GuestDday from '@/pages/MainPage/GuestDday';
 
 interface DdayProps {
   isLoggedIn?: boolean;
@@ -16,33 +18,7 @@ const Dday = ({ isLoggedIn = false }: DdayProps) => {
       {/* TODO
       - D-Day 설정 모달 추가
       - 비로그인일 때 클릭 시 로그인 페이지, 로그인일 때 클릭 시 D-Day 설정 모달 띄우기*/}
-      {isLoggedIn ? (
-        <Flex
-          padding="10px 20px"
-          color="white"
-          fontWeight="bold"
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Text fontSize="2xl">데브코스 수료식</Text>
-          <Box>
-            <Text fontSize="2xl">D-83</Text>
-            <Text fontSize="lg" fontWeight="normal">
-              2024.03.25
-            </Text>
-          </Box>
-        </Flex>
-      ) : (
-        <Flex
-          padding="10px 20px"
-          color="white"
-          fontWeight="bold"
-          direction="column"
-        >
-          <Text fontSize="2xl">D-Day를 등록해보세요.</Text>
-          <Text fontSize="md">D-Day는 로그인 후 등록 가능합니다.</Text>
-        </Flex>
-      )}
+      {isLoggedIn ? <LoginDday /> : <GuestDday />}
     </Box>
   );
 };
