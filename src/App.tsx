23d22165aeaaabb7f08dba5b33c3a52852b3436b
seunghyun <tmdgus7820@naver.com>
2024-01-03 14:31:48 +0900

    🚚 SignUp 디렉토리 pages로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
-import SignUp from '@/components/user/SignUp';
+import SignUp from '@/pages/user/SignUp';
 
 const App = () => {
   return (
