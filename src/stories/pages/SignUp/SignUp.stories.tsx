303c0d76a17daa7b47bc416dc74dfa4e5ee4b44d
seunghyun <tmdgus7820@naver.com>
2024-01-03 16:34:26 +0900

    ⚡️ SignUp(회원가입) Storybook 에 queryClient 에러 처리 추가

--- src/stories/pages/SignUp/SignUp.stories.tsx
+++ src/stories/pages/SignUp/SignUp.stories.tsx
@@ -1,9 +1,30 @@
-import SignUp from '@/pages/user/SignUp';
+import SignUp from '@/pages/SignUp';
 import { Meta, StoryObj } from '@storybook/react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { withRouter } from 'storybook-addon-react-router-v6';
 
-const queryclient = new QueryClient();
+const queryclient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      if (!query.meta?.errorMessage) {
+        return;
+      }
+
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 401 || error.response?.status === 400) {
+          return;
+        }
+
+        alert(error.message);
+      }
+    },
+  }),
+});
 
 const meta = {
   component: SignUp,
