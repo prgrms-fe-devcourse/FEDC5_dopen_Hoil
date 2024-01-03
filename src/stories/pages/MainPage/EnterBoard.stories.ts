f3f4e0d430c6c17c34cece4422078ffdfba0f71a
JaeHyunGround <gothddlek@naver.com>
2024-01-03 10:21:18 +0900

    🚚 BoardListPreview로 컴포넌트 이름 변경

--- src/stories/pages/MainPage/EnterBoard.stories.ts
+++ src/stories/pages/MainPage/EnterBoard.stories.ts
@@ -1,11 +1,11 @@
-import EnterBoard from '@/pages/MainPage/EnterBoard';
+import BoardListPreview from '@/pages/MainPage/BoardListPreview';
 import { Meta, StoryObj } from '@storybook/react';
 
-const meta: Meta<typeof EnterBoard> = {
-  component: EnterBoard,
+const meta: Meta<typeof BoardListPreview> = {
+  component: BoardListPreview,
 };
 
 export default meta;
-type Story = StoryObj<typeof EnterBoard>;
+type Story = StoryObj<typeof BoardListPreview>;
 
 export const Deafult: Story = {};
