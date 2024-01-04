dc4f37c6eccfb25050553bd6e323ad0a77098ee9
이종혁 <whdgur57117@gmail.com>
2024-01-04 13:42:11 +0900

    🎨 데이터 정규화 로직 query로 이동

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,30 +1,15 @@
 import { useMessageList } from '@/hooks/useMessageList';
-import { calculateTimeDiff } from './calculateTimeDiff';
 import UserContentBlockList from '@/components/common/UserContentBlockList';
 
 const MessageList = () => {
-  const { isLoading, error, data: messageList } = useMessageList();
-  const userDataList = messageList?.map(
-    ({ createdAt, message, sender, receiver }) => {
-      //TODO : 로그인한 유저의 정보 받아와서 비교
-      const otherType = sender._id === 'test' ? receiver : sender;
-      return {
-        coverImage: otherType.coverImage,
-        username: otherType.username,
-        content: message,
-        subContent: calculateTimeDiff(createdAt) || '',
-      };
-    },
-  );
-  //TODO : 에러처리 수정 필요
+  const { isLoading, error, userDataList } = useMessageList();
+  //TODO: 로딩 및 에러처리 세부 구현
   if (isLoading) {
-    return <div>로딩중</div>;
+    return <div>로딩중...</div>;
   }
-
   if (error) {
-    return <div>Error Occured</div>;
+    return <div>에러가 발생했습니다</div>;
   }
-
   return (
     <>{userDataList && <UserContentBlockList userDataList={userDataList} />}</>
   );
