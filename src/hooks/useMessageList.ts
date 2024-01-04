b87c97b0474d46f218f6cd2b2a74dc404213e3df
이종혁 <whdgur57117@gmail.com>
2024-01-04 13:42:11 +0900

    🎨 데이터 정규화 로직 query로 이동

--- src/hooks/useMessageList.ts
+++ src/hooks/useMessageList.ts
@@ -2,11 +2,21 @@ import { AxiosError } from 'axios';
 import { useQuery } from 'react-query';
 import { getMessageList } from '@/apis/message';
 import { Conversation } from '@/apis/type';
-
+import { calculateTimeDiff } from '@/components/MessageList/calculateTimeDiff';
 export const useMessageList = () => {
   const { isLoading, error, data } = useQuery<Conversation[], AxiosError>(
     'messageList',
     getMessageList,
   );
-  return { isLoading, error, data };
+  const userDataList = data?.map(({ createdAt, message, sender, receiver }) => {
+    //TODO : 로그인한 유저의 실제 _id 받아와서 비교
+    const otherType = sender._id === 'test' ? receiver : sender;
+    return {
+      coverImage: otherType.coverImage,
+      username: otherType.username,
+      content: message,
+      subContent: calculateTimeDiff(createdAt) || '',
+    };
+  });
+  return { isLoading, error, userDataList };
 };
