5fee26f01c81d38e6170d738522dc41f0b6258d6
이종혁 <whdgur57117@gmail.com>
2024-01-04 11:39:22 +0900

    💄 Route 최상단에 Layout 컴포넌트 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
+import MessageList from '@/components/MessageList';
+import Message from '@/components/Message/index';
+import PageLayout from '@/components/PageLayout/index';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route element={<PageLayout />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/message" element={<MessageList />} />
+          <Route path="/message/:userId" element={<Message />} />
+        </Route>
       </Routes>
     </>
   );
