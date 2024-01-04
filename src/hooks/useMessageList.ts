831b4e1a03363b9935ab560da723c24d5ed127c8
이종혁 <whdgur57117@gmail.com>
2024-01-04 19:56:02 +0900

    🎨 data 속성명 변경

--- src/hooks/useMessageList.ts
+++ src/hooks/useMessageList.ts
@@ -12,7 +12,7 @@ export const useMessageList = () => {
     //TODO : 로그인한 유저의 실제 _id 받아와서 비교
     const otherType = sender._id === 'test' ? receiver : sender;
     return {
-      coverImage: otherType.coverImage,
+      userImage: otherType.coverImage,
       username: otherType.username,
       content: message,
       subContent: calculateTimeDiff(createdAt) || '',
