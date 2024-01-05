3577980106ea3a83c9506d5ef84df4059147f945
이종혁 <whdgur57117@gmail.com>
2024-01-05 13:48:20 +0900

    ✨ 메세지 페이지 추가

---
+++ src/pages/MessageListPage/index.tsx
@@ -0,0 +1,13 @@
+import PageHeader from '@/components/PageHeader';
+import MessageList from '@/components/MessageList';
+
+const MessageListPage = () => {
+  return (
+    <>
+      <PageHeader pageName="메세지" />
+      <MessageList />;
+    </>
+  );
+};
+
+export default MessageListPage;
---
+++ src/pages/MessagePage/index.tsx
@@ -0,0 +1,12 @@
+import PageHeader from '@/components/PageHeader';
+import Message from '@/components/Message';
+const MessagePage = () => {
+  return (
+    <>
+      <PageHeader pageName="DM" />
+      <Message></Message>
+    </>
+  );
+};
+
+export default MessagePage;
