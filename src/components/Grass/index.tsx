bf4cb4e9bd3dd6bb9bbb047989c1b3b4a75914a2
loevray <rei980213@gmail.com>
2024-01-05 13:50:04 +0900

    :sparkles: Grass컴포넌트 storybook 작성완료

--- src/components/Grass/index.tsx
+++ src/components/Grass/index.tsx
@@ -1,6 +1,13 @@
 import { Box } from '@chakra-ui/react';
 
 const Grass = () => {
-  return <Box bg="rgba(255, 0, 0)">ㅎㅇ</Box>;
+  return (
+    <>
+      <Box bg="rgba(255, 0, 0, 1)">ㅎㅇ</Box>
+      <Box bg="rgba(255, 0, 0, 0.75)">ㅎㅇ</Box>
+      <Box bg="rgba(255, 0, 0, 0.5)">ㅎㅇ</Box>
+      <Box bg="rgba(255, 0, 0, 0.25)">ㅎㅇ</Box>
+    </>
+  );
 };
 export default Grass;
