18331427e91eed691bec6002cc00774b8c144c75
seunghyun <tmdgus7820@naver.com>
2024-01-06 03:19:38 +0900

    ✨ Account 페이지 추가(초기 인증 사용자 정보가 있을 때 UpdateUserInfo 페이지 보여주기)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,7 @@ import MainPage from '@/pages/MainPage';
 import SignUp from '@/pages/SignUp';
 import Login from '@/pages/Login';
 import MyPage from '@/pages/MyPage';
+import Account from '@/pages/MyPage/Account';
 import MessagePage from '@/pages/MessagePage';
 import MessageListPage from '@/pages/MessageListPage';
 import PageLayout from '@/components/PageLayout';
@@ -10,7 +11,6 @@ import ErrorPage from '@/pages/404Page';
 import SearchPage from '@/pages/SearchPage';
 import BoardEnterPage from '@/pages/BoardEnterPage';
 import BoardPage from '@/pages/BoardPage';
-import UpdateUserInfo from '@/pages/MyPage/UpdateUserInfo';
 
 const App = () => {
   return (
@@ -20,7 +20,7 @@ const App = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/mypage" element={<MyPage />} />
-          <Route path="/mypage/account" element={<UpdateUserInfo />} />
+          <Route path="/mypage/account" element={<Account />} />
           <Route path="/" element={<MainPage />} />
           <Route path="/message" element={<MessageListPage />} />
           <Route path="/message/:userId" element={<MessagePage />} />
