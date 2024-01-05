3dc4a296d7adc75878799473bd1f7624f9e1a939
이종혁 <whdgur57117@gmail.com>
2024-01-05 14:13:23 +0900

    🐛 git 충돌 해결

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,14 +2,21 @@ import { Route, Routes } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
 import SignUp from '@/pages/SignUp';
 import MyPage from '@/pages/MyPage';
+import MessagePage from '@/pages/MessagePage';
+import MessageListPage from '@/pages/MessageListPage';
+import PageLayout from '@/components/PageLayout';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/mypage" element={<MyPage />} />
+        <Route element={<PageLayout />}>
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/" element={<MainPage />} />
+          <Route path="/message" element={<MessageListPage />} />
+          <Route path="/message/:userId" element={<MessagePage />} />
+        </Route>
       </Routes>
     </>
   );
