12fce053c1a010e0d76c0ad96e46be169c23b8bc
seunghyun <tmdgus7820@naver.com>
2024-01-06 03:19:38 +0900

    ✨ Account 페이지 추가(초기 인증 사용자 정보가 있을 때 UpdateUserInfo 페이지 보여주기)

---
+++ src/pages/MyPage/Account/index.tsx
@@ -0,0 +1,23 @@
+import useUserInfo from '@/hooks/useUserInfo';
+import UpdateUserInfo from '../UpdateUserInfo';
+
+const Account = () => {
+  const { image, email, fullName, username } = useUserInfo();
+
+  return (
+    <>
+      {email && fullName && username ? (
+        <UpdateUserInfo
+          image={image}
+          email={email}
+          fullName={fullName}
+          username={username}
+        />
+      ) : (
+        '인증되지 않은 사용자입니다.'
+      )}
+    </>
+  );
+};
+
+export default Account;
