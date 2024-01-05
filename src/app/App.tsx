2805111f5c170744c0ce0c2a106764cb09041348
seunghyun <tmdgus7820@naver.com>
2024-01-05 15:31:15 +0900

    ⚡️ Pull을 위한 중간 Update

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,6 +10,7 @@ import ErrorPage from '@/pages/404Page';
 import SearchPage from '@/pages/SearchPage';
 import BoardEnterPage from '@/pages/BoardEnterPage';
 import BoardPage from '@/pages/BoardPage';
+import UpdateUserInfo from '@/pages/MyPage/UpdateUserInfo';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<Login />} />
           <Route path="/mypage" element={<MyPage />} />
+          <Route path="/mypage/account" element={<UpdateUserInfo />} />
           <Route path="/" element={<MainPage />} />
           <Route path="/message" element={<MessageListPage />} />
           <Route path="/message/:userId" element={<MessagePage />} />
--- src/types/user.ts
+++ src/types/user.ts
@@ -21,6 +21,7 @@ export interface LoginInputProperty {
 }
 
 export interface UserInfoInput {
+  profileImage?: string;
   email: string;
   fullName: string;
   username: string;
