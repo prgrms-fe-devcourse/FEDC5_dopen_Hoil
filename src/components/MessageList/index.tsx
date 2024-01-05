69701eccaace9cf3e3993caa8a5bfa6c9b41a2b1
이종혁 <whdgur57117@gmail.com>
2024-01-05 20:46:33 +0900

    💄 Message 관련 컴포넌트 / 페이지 스타일 적용

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,21 +1,22 @@
 import { useNavigate } from 'react-router-dom';
-import { Flex } from '@chakra-ui/react';
+import { Flex, FlexProps } from '@chakra-ui/react';
 import { useMessageList } from '@/hooks/useMessageList';
 import UserContentBlock from '../common/UserContentBlock';
 
-const MessageList = () => {
+const MessageList = ({ ...props }: FlexProps) => {
   const navigate = useNavigate();
   const { isLoading, error, userDataList } = useMessageList();
   //TODO: 로딩 및 에러처리 세부 구현
   if (isLoading) {
     return <div>로딩중...</div>;
   }
+
   if (error) {
     return <div>에러가 발생했습니다</div>;
   }
 
   return (
-    <Flex flexDir="column" gap="5">
+    <Flex flexDir="column" overflowY="auto" {...props}>
       {userDataList?.map(
         ({ key, userImage, username, content, subContent, userId }) => {
           return (
--- src/pages/MessagePage/index.tsx
+++ src/pages/MessagePage/index.tsx
@@ -1,11 +1,12 @@
 import PageHeader from '@/components/PageHeader';
 import Message from '@/components/Message';
+import { Flex } from '@chakra-ui/react';
 const MessagePage = () => {
   return (
-    <>
+    <Flex flexDir="column">
       <PageHeader pageName="DM" />
-      <Message></Message>
-    </>
+      <Message gap="8" h="100vh" />
+    </Flex>
   );
 };
 
