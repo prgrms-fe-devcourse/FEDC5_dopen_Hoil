ba2d8b88146382430ad5d9af84ac4e1f1d6f94ef
이종혁 <whdgur57117@gmail.com>
2024-01-07 21:06:33 +0900

    ♻️  MessagePage 컴포넌트 경로 수정 및 props 변경

--- src/pages/MessagePage/index.tsx
+++ src/pages/MessagePage/index.tsx
@@ -1,11 +1,12 @@
-import PageHeader from '@/components/PageHeader';
-import Message from '@/components/Message';
 import { Flex } from '@chakra-ui/react';
+import PageHeader from '@/components/PageHeader';
+import Message from '@/pages/MessagePage/Message';
+
 const MessagePage = () => {
   return (
     <Flex flexDir="column">
       <PageHeader pageName="DM" />
-      <Message gap="8" minH="100vh" />
+      <Message gap="10" minH="100vh" bgColor="gray.300" />
     </Flex>
   );
 };
