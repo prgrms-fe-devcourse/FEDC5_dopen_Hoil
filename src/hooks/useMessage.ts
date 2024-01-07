dad16b47a6e930390d538819f6ca8ebd2db1e6a5
이종혁 <whdgur57117@gmail.com>
2024-01-07 21:04:00 +0900

    ✨ useMessage return 속성에 시간 관련 속성 추가

--- src/hooks/useMessage.ts
+++ src/hooks/useMessage.ts
@@ -1,21 +1,37 @@
-import { getMessageListByUser } from '@/apis/message';
 import { useQuery } from 'react-query';
+import { getMessageListByUser } from '@/apis/message';
+import { convertDateToString } from '@/utils/convertDateToString';
+
+interface Message {
+  time: string;
+  message: string;
+  _id: string;
+  type: 'sent' | 'received';
+}
 
 export const useMessage = (userId: string) => {
-  //data는 시간을 기준으로 오름차순 정렬되어서 나옴
   const { isLoading, error, data } = useQuery(
     'message',
     async () => await getMessageListByUser(userId),
   );
 
-  const messageLogs = data?.map(({ message, sender, receiver, _id }) => {
-    if (import.meta.env.VITE_APP_TESTID === sender._id) {
-      return { _id, message, type: 'sent' };
-    }
-    if (import.meta.env.VITE_APP_TESTID === receiver._id) {
-      return { _id, message, type: 'received' };
+  const messageLogs = new Map<string, Message[]>();
+
+  data?.forEach(({ message, sender, _id, createdAt }) => {
+    const { date, time } = convertDateToString(new Date(createdAt));
+    const type =
+      import.meta.env.VITE_APP_TESTID === sender._id ? 'sent' : 'received';
+
+    const existingMessages = messageLogs.get(date);
+    if (existingMessages) {
+      messageLogs.set(date, [
+        ...existingMessages,
+        { _id, message, type, time },
+      ]);
+    } else {
+      messageLogs.set(date, [{ _id, message, type, time }]);
     }
   });
 
-  return { isLoading, error, messageLogs };
+  return { isLoading, error, messageLogs: Array.from(messageLogs.entries()) };
 };
