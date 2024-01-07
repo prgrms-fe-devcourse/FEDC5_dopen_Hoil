51373ddda88ce8fd28add01a6a55d8bb55879195
이종혁 <whdgur57117@gmail.com>
2024-01-08 03:54:55 +0900

    💄 Message 컴포넌트 높이 관련 스타일 수정

--- src/pages/MessagePage/index.tsx
+++ src/pages/MessagePage/index.tsx
@@ -1,12 +1,12 @@
-import { Flex } from '@chakra-ui/react';
 import PageHeader from '@/components/PageHeader';
 import Message from '@/pages/MessagePage/Message';
+import { Flex } from '@chakra-ui/react';
 
 const MessagePage = () => {
   return (
-    <Flex flexDir="column" h="100vh">
-      <PageHeader pageName="DM" />
-      <Message bgColor="gray.300" minH="100vh" overflowY="auto" />
+    <Flex flexDir="column" h="100%">
+      <PageHeader pageName="DM" flexShrink="0" />
+      <Message flexGrow="1" bgColor="gray.300" overflowY="auto" />
     </Flex>
   );
 };
