8dcf63153e5f39806c77a6ba2f90df3699fa3c15
이종혁 <whdgur57117@gmail.com>
2024-01-08 02:13:11 +0900

    🐛 getDay -> getDate로 수정

--- src/utils/convertDateToString.ts
+++ src/utils/convertDateToString.ts
@@ -1,7 +1,7 @@
 export const convertDateToString = (date: Date) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
-  const day = date.getDay();
+  const day = date.getDate();
   const time = date.toLocaleTimeString('ko-KR', { hour12: true });
   return {
     date: `${year}-${month}-${day}`,
