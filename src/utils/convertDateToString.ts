14554d43624e8806c22ca569d34eee54778a7066
이종혁 <whdgur57117@gmail.com>
2024-01-08 20:29:26 +0900

    ♻️ 시간 표시방식 인자값에 따라 변경 가능하게 수정 및 테스트 코드 변경

--- src/utils/convertDateToString.ts
+++ src/utils/convertDateToString.ts
@@ -1,8 +1,16 @@
-export const convertDateToString = (date: Date) => {
+/**
+ *
+ *
+ * @param hour12
+ * true(default)로 설정 시 오전/오후 표시 ex)오전 12:36:14
+ * false로 설정 시 오전/오후 표시 X ex)12시 36분 14초
+ *
+ */
+export const convertDateToString = (date: Date, hour12: boolean = true) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  const time = date.toLocaleTimeString('ko-KR', { hour12: true });
+  const time = date.toLocaleTimeString('ko-KR', { hour12 });
   return {
     date: `${year}-${month}-${day}`,
     time,
