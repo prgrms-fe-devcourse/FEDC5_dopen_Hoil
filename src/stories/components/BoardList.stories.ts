55972a39e1c8b020526110736e77c03d1918ac34
JaeHyunGround <gothddlek@naver.com>
2024-01-09 00:51:26 +0900

    💄 BoardList, BoardEnterPage storybook 수정

--- src/stories/components/BoardList.stories.ts
+++ src/stories/components/BoardList.stories.ts
@@ -3,9 +3,22 @@ import { Meta, StoryObj } from '@storybook/react';
 
 const meta: Meta<typeof BoardList> = {
   component: BoardList,
+  argTypes: {
+    width: {
+      control: 'text',
+    },
+    height: {
+      control: 'text',
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof BoardList>;
 
-export const Deafult: Story = {};
+export const Deafult: Story = {
+  args: {
+    width: '428px',
+    height: '40px',
+  },
+};
