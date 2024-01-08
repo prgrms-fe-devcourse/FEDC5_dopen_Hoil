e1e1077a8f8a2d2649491e21e567e998f02638af
seunghyun <tmdgus7820@naver.com>
2024-01-08 14:12:06 +0900

    ✨ useUserInfo hook을 useMyInfo로 변경(기존 useState,useEffect 사용 함수를 reactQuery를 통해 개선)

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -2,3 +2,4 @@ export const POST_LIST = 'post-list';
 export const USER_LIST = 'user-list';
 export const MESSAGE = 'message';
 export const MESSAGE_LIST = 'message-list';
+export const MY_INFO = 'my-info';
--- src/pages/MyPage/Account/index.tsx
+++ src/pages/MyPage/Account/index.tsx
@@ -1,21 +1,28 @@
-import useUserInfo from '@/hooks/useUserInfo';
+import useMyInfo from '@/hooks/useMyInfo';
 import UpdateUserInfo from '../UpdateUserInfo';
+import { Box } from '@chakra-ui/react';
 
 const Account = () => {
-  const { image, email, fullName, username } = useUserInfo();
+  const { myInfo, isError, isLoading } = useMyInfo();
+
+  if (isLoading) {
+    return <Box>로딩중입니다...</Box>;
+  }
+
+  if (isError || !myInfo) {
+    return <Box>인증되지 않은 사용자 입니다.</Box>;
+  }
+
+  const { image, email, fullName, username } = myInfo;
 
   return (
     <>
-      {email && fullName ? (
-        <UpdateUserInfo
-          image={image}
-          email={email}
-          fullName={fullName}
-          username={username}
-        />
-      ) : (
-        '인증되지 않은 사용자입니다.'
-      )}
+      <UpdateUserInfo
+        image={image}
+        email={email}
+        fullName={fullName}
+        username={username}
+      />
     </>
   );
 };
