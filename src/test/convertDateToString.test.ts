afefd58bd5fd5d11ea8ba7cbec3cb8f8687872fb
이종혁 <whdgur57117@gmail.com>
2024-01-08 20:29:26 +0900

    ♻️ 시간 표시방식 인자값에 따라 변경 가능하게 수정 및 테스트 코드 변경

--- src/test/convertDateToString.test.ts
+++ src/test/convertDateToString.test.ts
@@ -6,4 +6,8 @@ test('Converts a date type to an object with date and time properties."', () => 
     date: '2022-1-4',
     time: '오전 4:32:00',
   });
+  expect(convertDateToString(new Date('2022-01-04T04:32:00'), false)).toEqual({
+    date: '2022-1-4',
+    time: '4시 32분 0초',
+  });
 });
