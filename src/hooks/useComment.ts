33c37ac4c22901484b6ee17261d58b0b74aa8cf5
seunghyun <tmdgus7820@naver.com>
2024-01-09 20:08:40 +0900

    ✨ UseMyComment hook 내부 로직 수정(반환 값 Comment[] 추론 되도록 타입 가드 설정)

--- src/hooks/useComment.ts
+++ src/hooks/useComment.ts
@@ -2,11 +2,24 @@ import { useMutation, useQuery } from 'react-query';
 import { checkAuthenticated } from '@/apis/authentication';
 import { deleteComment } from '@/apis/comment';
 import { getUserInfo } from '@/apis/userInfo';
+import { Comment } from '@/apis/type';
 
 export const useMyComment = () => {
-  return useQuery('my-comment', async () => {
+  return useQuery<Comment[]>('my-comment', async () => {
     const { _id } = await checkAuthenticated();
-    return await getUserInfo(_id);
+    const userInfo = await getUserInfo(_id);
+
+    const isCommentArray = (
+      array: string[] | Comment[],
+    ): array is Comment[] => {
+      return array.length === 0 || typeof array[0] !== 'string';
+    };
+
+    if (!isCommentArray(userInfo.comments)) {
+      throw new Error('반환 값이 일치하지 않습니다.');
+    }
+
+    return userInfo.comments;
   });
 };
 
