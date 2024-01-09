c810b9664a0d8701dfc0155527b9d0e969ea87a5
seunghyun <tmdgus7820@naver.com>
2024-01-09 21:53:40 +0900

    🎨 UseMyComment Query Key 상수로 변경

--- src/hooks/useComment.ts
+++ src/hooks/useComment.ts
@@ -3,9 +3,10 @@ import { checkAuthenticated } from '@/apis/authentication';
 import { deleteComment } from '@/apis/comment';
 import { getUserInfo } from '@/apis/userInfo';
 import { Comment } from '@/apis/type';
+import { MY_COMMENT_LIST } from '@/constants/queryKeys';
 
 export const useMyComment = () => {
-  return useQuery<Comment[]>('my-comment', async () => {
+  return useQuery<Comment[]>(MY_COMMENT_LIST, async () => {
     const { _id } = await checkAuthenticated();
     const userInfo = await getUserInfo(_id);
 
