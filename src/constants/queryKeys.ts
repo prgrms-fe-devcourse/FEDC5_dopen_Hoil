5a2a435255bc0c0002fc79d6b12ece9769d30d6b
seunghyun <tmdgus7820@naver.com>
2024-01-09 21:53:40 +0900

    🎨 UseMyComment Query Key 상수로 변경

--- src/constants/queryKeys.ts
+++ src/constants/queryKeys.ts
@@ -4,3 +4,4 @@ export const MESSAGE = 'message';
 export const MESSAGE_LIST = 'message-list';
 export const MY_INFO = 'my-info';
 export const ONLINE_USER_LIST = 'online-user-list';
+export const MY_COMMENT_LIST = 'my-comment-list';
