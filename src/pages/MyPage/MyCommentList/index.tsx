1c0a288659087eddba8c67052ac0af62fa18305d
seunghyun <tmdgus7820@naver.com>
2024-01-09 20:09:12 +0900

    ⚡️ MyCommentList 페이지 UI/기능 작업

--- src/pages/MyPage/MyCommentList/index.tsx
+++ src/pages/MyPage/MyCommentList/index.tsx
@@ -1,84 +1,46 @@
-import { /*useMyComment,*/ useDeleteComment } from '@/hooks/useComment';
-// import { useMyInfo } from '@/hooks/useAuth';
-import styled from '@emotion/styled';
-import {
-  Box,
-  Text,
-  Flex,
-  Avatar,
-  UnorderedList,
-  ListItem,
-} from '@chakra-ui/react';
-import { comments } from './data';
+import { Box, UnorderedList } from '@chakra-ui/react';
+import { useMyComment } from '@/hooks/useComment';
+import { useMyInfo } from '@/hooks/useAuth';
 
 import PageHeader from '@/components/PageHeader';
 import Footer from '@/components/Footer';
+import MyCommentListItem from './MyCommentListItem';
 
 const MyCommentList = () => {
-  // const { data: myInfo} = useMyInfo();
-  // const { username, image } = myInfo;
+  const { data: myInfo } = useMyInfo();
+  const { data: myCommentList, isLoading, isError } = useMyComment();
 
-  // const { data: myCommentList, isLoading } = useMyComment();
+  if (isLoading) {
+    return <Box>로딩중입니다...</Box>;
+  }
 
-  // if (isLoading) {
-  //   return <div>로딩중입니다...</div>;
-  // }
-  const { mutate } = useDeleteComment();
-  const onDeleteComment = (title: string, id: string) => {
-    if (confirm(`${title}을 정말 삭제하시겠습니까?`)) {
-      // 제거
-      mutate(id);
-    }
-  };
+  if (isError || !myCommentList) {
+    return <Box>예상치 못한 오류가 발생했습니다.</Box>;
+  }
+
+  if (!myInfo) {
+    return <Box>예상치 못한 오류가 발생했습니다.</Box>;
+  }
+  const { username, image } = myInfo;
 
   return (
     <Box>
       <PageHeader pageName="내가 작성한 댓글" />
       <UnorderedList listStyleType="none">
-        {comments.map((_id, comment) => (
-          <ListItem key={_id} p="10px 0 10px" borderBottom="1px solid #D9D9D9">
-            <Flex mb="10px" justifyContent="space-between">
-              <Flex alignItems="center">
-                <Box mr="10px">
-                  <Avatar w="40px" h="40px" /*src={image ?? '기본 이미지'}*/ />
-                </Box>
-                <Box>
-                  <Text as="strong" display="block" fontSize="1.4rem">
-                    {/* {username} */} 테스트
-                  </Text>
-                  <Text as="span" fontSize="sm">
-                    2일전
-                  </Text>
-                </Box>
-              </Flex>
-              <Box>
-                <Text
-                  as="a"
-                  fontSize="sm"
-                  color="gray.600"
-                  onClick={() => onDeleteComment(String(comment), _id)}
-                >
-                  삭제
-                </Text>
-              </Box>
-            </Flex>
-            <Box>
-              <CommentText>{comment}</CommentText>
-            </Box>
-          </ListItem>
+        {myCommentList.map(({ _id, comment }) => (
+          <MyCommentListItem
+            key={_id}
+            id={_id}
+            comment={comment}
+            image={image}
+            username={username}
+            lineClamp={3}
+          />
         ))}
       </UnorderedList>
       <Footer />
     </Box>
   );
 };
-
-const CommentText = styled.p`
-  display: -webkit-box;
-  -webkit-line-clamp: 3;
-  -webkit-box-orient: vertical;
-  overflow: hidden;
-  font-size: 1.2rem;
-`;
 
 export default MyCommentList;
