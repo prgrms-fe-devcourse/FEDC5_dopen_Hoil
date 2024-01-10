9c6b6f995f1fb666cc6f74700e62bf60e3aecdc3
loevray <rei980213@gmail.com>
2024-01-10 22:37:17 +0900

    :art: DdayModal컴포넌트에 react-hook-form 도입 - error message 관련 처리중

--- src/pages/MainPage/DdayModal/index.tsx
+++ src/pages/MainPage/DdayModal/index.tsx
@@ -8,33 +8,82 @@ import {
   InputGroup,
   InputRightElement,
 } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+
+interface DdayModalInputTypes {
+  dDayTitle: string;
+  dDayDate: Date;
+}
 
 const DdayModal = ({
   isOpen,
   onClose,
 }: Pick<MyModalProps, 'isOpen' | 'onClose'>) => {
+  const {
+    register,
+    resetField,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<DdayModalInputTypes>();
+
+  const onSuccess = () => {};
+
   return (
     <MyModal
       onClose={onClose}
       isOpen={isOpen}
       title="D-DAY"
       buttonText="D-day 등록하기"
-      onButtonClick={() => {}}
+      onButtonClick={() => handleSubmit(onSuccess)}
+      isCentered
     >
       <form style={{ padding: '0 20px' }}>
-        <FormControl>
-          <FormLabel p="12px 0">D-Day 명을 작성해주세요.</FormLabel>
+        <FormControl isInvalid={!!errors.dDayTitle}>
+          <FormLabel htmlFor="dDayTitle" p="12px 0">
+            D-Day 명을 작성해주세요.
+          </FormLabel>
           <InputGroup>
-            <Input h="40px" p="0 18px" />
+            <Input
+              id="dDayTitle"
+              h="40px"
+              p="0 18px"
+              {...register('dDayTitle', {
+                required: 'd-day 명을 작성해주세요!',
+                minLength: {
+                  value: 1,
+                  message: '최소 1글자 이상 입력해주세요',
+                },
+                maxLength: {
+                  value: 20,
+                  message: '최대 20글자까지 가능합니다',
+                },
+              })}
+            />
             <InputRightElement h="100%" right="18px">
-              <CloseButton bg="gray.700" borderRadius="50%" color="white" />
+              <CloseButton
+                bg="gray.700"
+                borderRadius="50%"
+                color="white"
+                onClick={() => resetField('dDayTitle')}
+              />
             </InputRightElement>
           </InputGroup>
-          <FormErrorMessage></FormErrorMessage>
+          <FormErrorMessage>{errors.dDayDate?.message}</FormErrorMessage>
         </FormControl>
-        <FormControl>
-          <FormLabel p="12px 0">날짜를 선택해주세요.</FormLabel>
-          <Input type="date" p="0 18px" h="40px" w="135px" />
+        <FormControl isInvalid={!!errors.dDayDate}>
+          <FormLabel htmlFor="dDayDate" p="12px 0">
+            날짜를 선택해주세요.
+          </FormLabel>
+          <Input
+            id="dDayDate"
+            type="date"
+            p="0 18px"
+            h="40px"
+            w="135px"
+            {...register('dDayDate', {
+              required: 'd-day날짜를 기입해주세요',
+            })}
+          />
           <FormErrorMessage></FormErrorMessage>
         </FormControl>
       </form>
