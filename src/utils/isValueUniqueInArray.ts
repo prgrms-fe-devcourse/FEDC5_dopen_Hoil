4c2ea2bfd10ce608be8bb7b96740ed2611c9f3d6
seunghyun <tmdgus7820@naver.com>
2024-01-10 23:40:01 +0900

    🐛 isValueUniqueInArray (부정문 ! 제거) 수정

--- src/utils/isValueUniqueInArray.ts
+++ src/utils/isValueUniqueInArray.ts
@@ -2,4 +2,4 @@ export const isValueUniqueInArray = <T, K extends keyof T>(
   array: T[],
   key: K,
   valueToCheck: T[K],
-): boolean => !array.some((item) => item[key] === valueToCheck);
+): boolean => array.some((item) => item[key] === valueToCheck);
