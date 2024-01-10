aa35481e1fda9e3474ee9edea7067707b992a9c8
loevray <rei980213@gmail.com>
2024-01-10 21:43:29 +0900

    :sparkles: D-day모달 UI작성 완료

--- src/pages/MainPage/LoginDday.tsx
+++ src/pages/MainPage/LoginDday.tsx
@@ -1,6 +1,21 @@
-import { Box, Flex, Text } from '@chakra-ui/react';
+import MyModal from '@/components/common/MyModal';
+import {
+  Box,
+  CloseButton,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
 
 const LoginDday = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Flex
       padding="10px 20px"
@@ -8,6 +23,7 @@ const LoginDday = () => {
       fontWeight="bold"
       alignItems="center"
       justifyContent="space-between"
+      onClick={onOpen}
     >
       <Text fontSize="2xl">데브코스 수료식</Text>
       <Box>
@@ -16,6 +32,31 @@ const LoginDday = () => {
           2024.03.25
         </Text>
       </Box>
+      <MyModal
+        onClose={onClose}
+        isOpen={isOpen}
+        title="D-DAY"
+        buttonText="D-day 등록하기"
+        onButtonClick={() => {}}
+      >
+        <form style={{ padding: '0 20px' }}>
+          <FormControl>
+            <FormLabel p="12px 0">D-Day 명을 작성해주세요.</FormLabel>
+            <InputGroup>
+              <Input h="40px" p="0 18px" />
+              <InputRightElement h="100%" right="18px">
+                <CloseButton bg="gray.700" borderRadius="50%" color="white" />
+              </InputRightElement>
+            </InputGroup>
+            <FormErrorMessage></FormErrorMessage>
+          </FormControl>
+          <FormControl>
+            <FormLabel p="12px 0">날짜를 선택해주세요.</FormLabel>
+            <Input type="date" p="0 18px" h="40px" w="135px" />
+            <FormErrorMessage></FormErrorMessage>
+          </FormControl>
+        </form>
+      </MyModal>
     </Flex>
   );
 };
