92de05da10afefb1b513b786ab2af232f629c833
seunghyun <tmdgus7820@naver.com>
2024-01-11 00:09:00 +0900

    🎨 MyBoardList 페이지(리뷰 반영, ReactQuery onSuccess 제거)

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -3,33 +3,18 @@ import { MYPOST_LIST, POST_DETAIL } from '@/constants/queryKeys';
 import { checkAuthenticated } from '@/apis/authentication';
 import { getPostDetail } from '@/apis/post';
 
-interface PostProps {
-  onSuccessFn?: () => void;
-  onErrorFn?: () => void;
-}
-
-interface PostDetailProps extends PostProps {
+interface PostDetailProps {
   id: string;
 }
 
 export const useMyPostList = () => {
-  return useQuery(
-    MYPOST_LIST,
-    async () => {
-      const { posts } = await checkAuthenticated();
+  return useQuery(MYPOST_LIST, async () => {
+    const { posts } = await checkAuthenticated();
 
-      return posts;
-    },
-    {},
-  );
+    return posts;
+  });
 };
 
-export const usePostDetail = ({ onSuccessFn, id }: PostDetailProps) => {
-  return useQuery(POST_DETAIL, async () => await getPostDetail(id), {
-    onSuccess: () => {
-      if (onSuccessFn) {
-        onSuccessFn();
-      }
-    },
-  });
+export const usePostDetail = ({ id }: PostDetailProps) => {
+  return useQuery(POST_DETAIL, async () => await getPostDetail(id), {});
 };
