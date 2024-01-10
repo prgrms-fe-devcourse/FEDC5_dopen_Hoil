c789077e8775cd3e365021e012670ec76069a243
loevray <rei980213@gmail.com>
2024-01-11 00:41:17 +0900

    :sparkles: Dday기능 완성 - calculateDday함수 제작 후 유틸폴더로 이동 - 로컬스토리지를 사용합니다

--- src/pages/MainPage/LoginDday.tsx
+++ src/pages/MainPage/LoginDday.tsx
@@ -1,8 +1,30 @@
 import { Box, Flex, Text, useDisclosure } from '@chakra-ui/react';
 import DdayModal from './DdayModal';
+import { useEffect, useMemo, useState } from 'react';
+import { getItem } from '@/utils/storage';
+import { calculateDday } from '@/utils/calculateDday';
 
 const LoginDday = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const [dDay, setDday] = useState({
+    dDayTitle: '데브코스 수료식',
+    dDayDate: '2024.03.25',
+  });
+
+  useEffect(() => {
+    const storedDday = getItem('d-day', {
+      dDayTitle: '데브코스 수료식',
+      dDayDate: '2024.03.25',
+    });
+    setDday(storedDday);
+  }, []);
+
+  const dDayRemain = useMemo(
+    () => calculateDday(dDay.dDayDate),
+    [dDay.dDayDate],
+  );
+
   return (
     <Flex
       padding="10px 20px"
@@ -12,14 +34,14 @@ const LoginDday = () => {
       justifyContent="space-between"
       onClick={onOpen}
     >
-      <Text fontSize="2xl">데브코스 수료식</Text>
+      <Text fontSize="2xl">{dDay.dDayTitle}</Text>
       <Box>
-        <Text fontSize="2xl">D-83</Text>
+        <Text fontSize="2xl">{`D-${dDayRemain}`}</Text>
         <Text fontSize="lg" fontWeight="normal">
-          2024.03.25
+          {dDay.dDayDate}
         </Text>
       </Box>
-      <DdayModal isOpen={isOpen} onClose={onClose} />
+      <DdayModal isOpen={isOpen} onClose={onClose} setDday={setDday} />
     </Flex>
   );
 };
