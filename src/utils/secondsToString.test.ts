7444fec4073097d2bfcba5e29c692c152b6e5570
loevray <rei980213@gmail.com>
2024-01-11 18:40:37 +0900

    :white_check_mark: stringTimeToSeconds 테스트파일 추가

--- src/utils/secondsToString.test.ts
+++ src/utils/secondsToString.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest';
 import { secondsToStringTime } from './secondsToStringTime';
 
-test('Converts a date type to an object with date and time properties."', () => {
+test('number type seconds Convert to string time HH:MM:SS."', () => {
   expect(secondsToStringTime(10000)).toEqual('02:46:40');
 });
