be3a080d617186e4811c387afd8274e0bb0e3f97
JaeHyunGround <gothddlek@naver.com>
2024-01-11 10:56:15 +0900

    ✨ MainPage 사용자 정보 불러오기

--- src/pages/MainPage/LoginProfile.tsx
+++ src/pages/MainPage/LoginProfile.tsx
@@ -1,27 +1,32 @@
+import { User } from '@/apis/type';
 import { DEFAULT_WIDTH } from '@/constants/style';
 import { Avatar, Flex, Text } from '@chakra-ui/react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const LoginProfile = () => {
+interface LoginProfileProps {
+  myInfo: User;
+}
+
+const LoginProfile = ({ myInfo }: LoginProfileProps) => {
+  const [advice, setAdvice] = useState('');
+
+  useEffect(() => {
+    axios
+      .get('https://api.adviceslip.com/advice')
+      .then((res) => setAdvice(res.data.slip.advice));
+  }, []);
+
   return (
-    <Flex alignItems="center" maxW={DEFAULT_WIDTH}>
-      <Avatar
-        size="2xl"
-        name="Kola Tioluwani"
-        src="https://bit.ly/tioluwani-kolawole"
-        cursor="default"
-      />
+    <Flex cursor="default" alignItems="center" maxW={DEFAULT_WIDTH}>
+      <Avatar size="2xl" name="Kola Tioluwani" src={myInfo.image || ''} />
       <Flex direction="column" marginLeft="20px">
-        <Text
-          width="fit-content"
-          fontSize="3xl"
-          fontWeight="bold"
-          _hover={{ cursor: 'pointer' }}
-        >
-          {/* TODO: 클릭 시 마이페이지로 가게 처리 필요 */}
-          <span style={{ color: '#F88585' }}>[사용자 이름]</span>님 안녕하세요!
+        <Text width="fit-content" fontSize="3xl" fontWeight="bold">
+          <span style={{ color: '#F88585' }}>{myInfo.username}</span>님
+          안녕하세요!
         </Text>
-        <Text fontStyle="oblique" fontSize="md" cursor="default">
-          [사용자가 설정한 명언]
+        <Text fontStyle="oblique" fontSize="md">
+          {advice}
         </Text>
       </Flex>
     </Flex>
