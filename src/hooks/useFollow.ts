9d4bb7246b3099f9de6ca5a2ced6335d29e8666a
seunghyun <tmdgus7820@naver.com>
2024-01-12 01:15:31 +0900

    🎨 UseFollow hook 수정

--- src/hooks/useFollow.ts
+++ src/hooks/useFollow.ts
@@ -4,16 +4,37 @@ import { CREATE_FOLLOW, DELETE_FOLLOW } from '@/constants/queryKeys';
 
 interface FollowProps {
   id: string;
+  onSuccessFn?: () => void;
 }
 
-export const useCreateFollow = ({ id }: FollowProps) => {
-  return useMutation(CREATE_FOLLOW, async () => {
-    await followUser(id);
-  });
+export const useCreateFollow = ({ id, onSuccessFn }: FollowProps) => {
+  return useMutation(
+    CREATE_FOLLOW,
+    async () => {
+      await followUser(id);
+    },
+    {
+      onSuccess: () => {
+        if (onSuccessFn) {
+          onSuccessFn();
+        }
+      },
+    },
+  );
 };
 
-export const useDeleteFollow = ({ id }: FollowProps) => {
-  return useMutation(DELETE_FOLLOW, async () => {
-    await unfollowUser(id);
-  });
+export const useDeleteFollow = ({ id, onSuccessFn }: FollowProps) => {
+  return useMutation(
+    DELETE_FOLLOW,
+    async () => {
+      await unfollowUser(id);
+    },
+    {
+      onSuccess: () => {
+        if (onSuccessFn) {
+          onSuccessFn();
+        }
+      },
+    },
+  );
 };
