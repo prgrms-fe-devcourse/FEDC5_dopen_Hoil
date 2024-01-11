a0a0aec16609fcd242187913212b3083f5e5478e
loevray <rei980213@gmail.com>
2024-01-11 18:09:12 +0900

    :bulg: useTodayTimePost에 주석 추가

--- src/hooks/useTodayTimePost.ts
+++ src/hooks/useTodayTimePost.ts
@@ -13,6 +13,8 @@ export const useTodayTimePost = (channelId: string) => {
       },
       select: (posts) =>
         posts.filter((post) => {
+          // toLocaleDateString으로 YYYY-M-D(예시 2024-1-8)로 변환 후
+          // 포스트 createdAt(YYYY-M-DTHH:MM:SS)로 넘어온 값을 T기준으로 잘라서 비교
           const currentDate = new Date().toLocaleDateString('en-CA');
           return post.createdAt.split('T')[0] === currentDate;
         })?.[0],
