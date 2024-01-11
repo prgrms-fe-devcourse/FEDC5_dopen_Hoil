aa5a0e32697d66c4df88ce676b6bdd4e87d8df80
JaeHyunGround <gothddlek@naver.com>
2024-01-11 16:40:46 +0900

    ✨ post 작성을 위해 서버에 게시판 등록, 게시판 목록을 API로 가져와 route 재설정, board 관련 페이지 API로 가데이터로 가공

--- src/constants/Board.ts
+++
@@ -1,9 +0,0 @@
-interface BoardListTypes {
-  [board: string]: string;
-}
-
-export const BOARD_LIST: BoardListTypes = {
-  free: '자유 게시판',
-  reflections: '인증 & 회고 게시판',
-  infoshare: '정보 공유 게시판',
-};
