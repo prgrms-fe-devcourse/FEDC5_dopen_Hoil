41a161459a67089dc9f2851b1143f0097760acce
이종혁 <whdgur57117@gmail.com>
2024-01-11 12:08:53 +0900

    ✨ useNotificationList message 변환로직 추가 및 return 속성 변경

--- src/hooks/useNotificationList.ts
+++ src/hooks/useNotificationList.ts
@@ -1,5 +1,22 @@
 import { useQuery } from 'react-query';
-import { getUserNotificationList } from '@/apis/notifications';
+import {
+  getUserNotificationList,
+  NotificationType,
+} from '@/apis/notifications';
+import { User } from '@/apis/type';
+export interface MyNotificationListItem {
+  type: NotificationType;
+  author: User;
+  date: string; //UTC
+  _id: string;
+}
+
+export const messageByTypes: { [key in NotificationType]: string } = {
+  MESSAGE: '회원님에게 메시지를 보냈습니다.',
+  COMMENT: '회원님의 게시물에 댓글을 남겼습니다.',
+  FOLLOW: '회원님을 팔로우하기 시작했습니다 ',
+  LIKE: '회원님의 게시물에 좋아요를 보냈습니다',
+};
 
 export const useNotificationList = () => {
   const { data, isLoading, error } = useQuery(
@@ -10,26 +27,32 @@ export const useNotificationList = () => {
     },
   );
 
-  const notificationList = data?.map((notify) => {
-    //TODO : FOLLOW 및 LIKE에 대한 알림 확인 필요
-    const { username } = notify.author;
-    const date = notify.createdAt;
+  const initialData = data ?? [];
 
-    if (notify.message) {
-      return { type: 'MESSAGE', username, date };
-    }
+  const myNotificationList: MyNotificationListItem[] = initialData.map(
+    (notify) => {
+      //TODO : FOLLOW 및 LIKE에 대한 알림 확인 필요
 
-    if (notify.comment) {
-      return { type: 'COMMENT', username, date };
-    }
+      const { createdAt: date, _id, author } = notify;
 
-    if (notify.follow) {
-      return { type: 'FOLLOW', username, date };
-    }
-  });
+      if (notify.message) {
+        return { type: 'MESSAGE', author, date, _id };
+      }
+
+      if (notify.comment) {
+        return { type: 'COMMENT', author, date, _id };
+      }
+
+      if (notify.follow) {
+        return { type: 'FOLLOW', author, date, _id };
+      } else {
+        throw new Error('notify type is invalid!');
+      }
+    },
+  );
 
   return {
-    notificationList,
+    myNotificationList,
     isLoading,
     error,
   };
