2089f55c5902f4c6c6ae3ef4a8ccd253a3f7117c
loevray <rei980213@gmail.com>
2024-01-11 18:38:03 +0900

    :bug: viteset.fakeTime이용하여 calculateTimeDiff데이트 객체 모킹

--- src/utils/calculateTimeDiff.test.ts
+++ src/utils/calculateTimeDiff.test.ts
@@ -1,7 +1,8 @@
-import { expect, test } from 'vitest';
+import { expect, test, vitest } from 'vitest';
 import { calculateTimeDiff } from '@/utils/calculateTimeDiff';
 
 test('calculate time difference between UTC type', () => {
+<<<<<<< HEAD
 <<<<<<< HEAD:src/test/calculateTimeDiff.test.ts
   expect(
     calculateTimeDiff(
@@ -12,4 +13,9 @@ test('calculate time difference between UTC type', () => {
 =======
   expect(calculateTimeDiff('2024-01-05T17:01:05.147Z')).toBe('5일 전');
 >>>>>>> c365f3e (:poop: 많은 변경사항 발생...본문을 봐주세요):src/utils/calculateTimeDiff.test.ts
+=======
+  const fakeDate = new Date('2024-01-07T17:01:05.147Z');
+  vitest.useFakeTimers().setSystemTime(fakeDate);
+  expect(calculateTimeDiff('2024-01-05T17:01:05.147Z')).toBe('2일 전');
+>>>>>>> d061930 (:bug: viteset.fakeTime이용하여 calculateTimeDiff데이트 객체 모킹)
 });
