8dc35282c5337fad8ce0c816507b3082eb710fe6
seunghyun <tmdgus7820@naver.com>
2024-01-11 23:06:08 +0900

    ✨ Comment 컴포넌트 Storybook 추가

---
+++ src/stories/components/Comment.stories.tsx
@@ -0,0 +1,58 @@
+import Comment from '@/components/Comment';
+import { Meta, StoryObj } from '@storybook/react';
+import { AxiosError } from 'axios';
+import { QueryCache, QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      if (!query.meta?.errorMessage) {
+        return;
+      }
+
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 401 || error.response?.status === 400) {
+          return;
+        }
+
+        alert(error.message);
+      }
+    },
+  }),
+});
+
+const meta = {
+  component: Comment,
+  decorators: [
+    (Story) => (
+      <QueryClientProvider client={queryClient}>
+        <Story />
+      </QueryClientProvider>
+    ),
+  ],
+  argTypes: {
+    keyword: {
+      control: 'text',
+    },
+    offset: {
+      control: 'number',
+    },
+    limit: {
+      control: 'number',
+    },
+    isDivider: {
+      control: 'boolean',
+    },
+  },
+} satisfies Meta<typeof Comment>;
+
+export default meta;
+
+type Story = StoryObj<typeof Comment>;
+
+export const Deafult: Story = {};
