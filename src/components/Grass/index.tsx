5108c67b31947322b8371797b4c3124f2d9a3dc4
loevray <rei980213@gmail.com>
2024-01-13 01:09:51 +0900

    :art: Grass컴포넌트에서 map이용하여 GrassCell을 뿌려줌(Grid, GridItem관계)

--- src/components/Grass/index.tsx
+++ src/components/Grass/index.tsx
@@ -1,7 +1,27 @@
-import { Container } from '@chakra-ui/react';
+import { Grid } from '@chakra-ui/react';
+import GrassCell from './GrassCell';
 
 const Grass = () => {
-  return <Container>나는 그래스</Container>;
+  const today = new Date();
+  const lastDay = new Date(
+    today.getFullYear(),
+    today.getMonth() + 1,
+    0,
+  ).getDate();
+  //게시글 filter로 뽑아와서 1월달 글 존재하면 percentage계산해서 배열화하면 됨
+  const thisMonthTimerData = [...Array(lastDay)];
+  return (
+    <Grid
+      templateColumns="repeat(7, fit-content(20px))"
+      gap={2}
+      p={4}
+      maxW="428px"
+    >
+      {thisMonthTimerData.map((_, index) => (
+        <GrassCell key={`day-${index}`} percentage={1} />
+      ))}
+    </Grid>
+  );
 };
 
 export default Grass;
