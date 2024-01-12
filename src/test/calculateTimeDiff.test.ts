e6fc8cd32c7b66d98649126b25217a0c4fa4fe1d
이종혁 <whdgur57117@gmail.com>
2024-01-12 09:14:32 +0900

    🐛 calculateTimeDiff test코드 오류 수정

--- src/test/calculateTimeDiff.test.ts
+++ src/test/calculateTimeDiff.test.ts
@@ -2,5 +2,10 @@ import { expect, test } from 'vitest';
 import { calculateTimeDiff } from '@/utils/calculateTimeDiff';
 
 test('calculate time difference between UTC type', () => {
-  expect(calculateTimeDiff('2024-01-05T17:01:05.147Z')).toBe('2일 전');
+  expect(
+    calculateTimeDiff(
+      '2024-01-05T17:01:05.147Z',
+      new Date('2024-01-12 00:12:02 UTC'),
+    ),
+  ).toBe('6일 전');
 });
