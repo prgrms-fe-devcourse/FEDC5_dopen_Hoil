3709e57c721508919f05484835240d05c9d6a3d4
JaeHyunGround <gothddlek@naver.com>
2024-01-12 13:44:29 +0900

    💄 OnlineUsers Text 길이가 길어질 시 ... 처리

--- src/pages/BoardPage/OnlineUsers.tsx
+++ src/pages/BoardPage/OnlineUsers.tsx
@@ -22,7 +22,7 @@ const OnlineUsers = () => {
           접속 중인 사용자가 없습니다.
         </Text>
       ) : (
-        <Flex gap="10px" overflowX="auto">
+        <Flex gap="15px" overflowX="auto">
           {onlineUsersListData?.map((onlineUser) => (
             <OnlineUserProfile
               key={onlineUser._id}
