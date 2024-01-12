6974ef9b743cd1497abf4de6606951d8591f5652
loevray <rei980213@gmail.com>
2024-01-12 13:55:45 +0900

    :art: GrassCell컴포넌트 Box에서 GridItem으로 컨테이너 변경

--- src/components/Grass/GrassCell.tsx
+++ src/components/Grass/GrassCell.tsx
@@ -1,15 +1,15 @@
-import { Box, BoxProps } from '@chakra-ui/react';
+import { GridItem, GridItemProps } from '@chakra-ui/react';
 
-interface GrassCellProps extends BoxProps {
+interface GrassCellProps extends GridItemProps {
   percentage: number; //0, 0.25, 5, 0.75, 1
 }
 
 const GrassCell = ({ percentage = 0, ...props }: GrassCellProps) => {
   return (
-    <Box
+    <GridItem
       bg={`rgba(255,0,0, ${percentage})`}
       border="1px solid black"
-      size="5px"
+      boxSize="50px"
       {...props}
     />
   );
