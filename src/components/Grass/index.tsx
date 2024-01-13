bd3188362860cd73be20b4b0f07da009f2c0618e
loevray <rei980213@gmail.com>
2024-01-13 17:06:58 +0900

    :art: Grass컴포넌트 더미데이터 프로퍼티 변경 title => time

--- src/components/Grass/index.tsx
+++ src/components/Grass/index.tsx
@@ -2,14 +2,14 @@ import { Grid, GridProps } from '@chakra-ui/react';
 import GrassCell from './GrassCell';
 
 interface GrassProps extends GridProps {
-  timerPosts: { title: string; createdAt: string }[]; // 추후 Post[]로 변경필요
+  timerPosts: { time: string; createdAt: string }[]; // 추후 Post[]로 변경필요
 }
 
 const DUMMY_DATA = [
-  { title: '06:07:00', createdAt: '2024-01-01T20:48:19.816Z' },
-  { title: '02:00:00', createdAt: '2024-01-04T20:48:19.816Z' },
-  { title: '06:07:00', createdAt: '2024-01-05T20:48:19.816Z' },
-  { title: '12:07:00', createdAt: '2024-01-10T20:48:19.816Z' },
+  { time: '06:07:00', createdAt: '2024-01-01T20:48:19.816Z' },
+  { time: '02:00:00', createdAt: '2024-01-04T20:48:19.816Z' },
+  { time: '06:07:00', createdAt: '2024-01-05T20:48:19.816Z' },
+  { time: '12:07:00', createdAt: '2024-01-10T20:48:19.816Z' },
 ];
 
 const Grass = ({ timerPosts = DUMMY_DATA }: GrassProps) => {
@@ -21,6 +21,7 @@ const Grass = ({ timerPosts = DUMMY_DATA }: GrassProps) => {
   ).getDate();
   //회고 게시글을 이번 달 기준으로 filter해서 가져온다. 글 존재하면 percentage계산해서 배열화하면 됨
   const thisMonthTimerData = Array(lastDay).fill(0);
+
   timerPosts.forEach(({ createdAt }) => {
     const [, , createdDay] = new Date(createdAt)
       .toLocaleDateString('en-CA', {
@@ -33,6 +34,7 @@ const Grass = ({ timerPosts = DUMMY_DATA }: GrassProps) => {
     //0번 인덱스 기준이라 날짜에서 1을 빼줍니다
     thisMonthTimerData[+createdDay - 1] = 1;
   });
+
   return (
     <Grid templateColumns="repeat(7, fit-content(20px))" gap="5px">
       {thisMonthTimerData.map((isRecorded, index) => (
