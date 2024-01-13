0925b90464e67fc90919a716ed1c1c38d60b322f
loevray <rei980213@gmail.com>
2024-01-13 15:31:50 +0900

    :fire: Footer컴포넌트에서 랭킹제거

--- src/stories/components/Footer.stories.ts
+++ src/stories/components/Footer.stories.ts
@@ -1,10 +1,8 @@
 import Footer from '@/components/Footer';
 import { Meta, StoryObj } from '@storybook/react';
-import { withRouter } from 'storybook-addon-react-router-v6';
 
 const meta: Meta<typeof Footer> = {
   component: Footer,
-  decorators: [withRouter],
   argTypes: {
     width: {
       control: 'text',
