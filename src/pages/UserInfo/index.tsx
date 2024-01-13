7c89ae9f7d91f3f875d1114839ee4c734a8b6be6
seunghyun <tmdgus7820@naver.com>
2024-01-13 23:13:08 +0900

    ⚡️ UserInfo 페이지 (Profile, stateMessage, followInfo 컴포넌트 분리)

--- src/pages/UserInfo/index.tsx
+++ src/pages/UserInfo/index.tsx
@@ -1,33 +1,39 @@
 import { useParams } from 'react-router-dom';
-import { Box, Heading } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
+import { useMyInfo } from '@/hooks/useAuth';
 import { useGetUsersList } from '@/hooks/useUser';
 import { isValueUniqueInArray } from '@/utils/isValueUniqueInArray';
 
 import PageHeader from '@/components/PageHeader';
 import ErrorPage from '@/pages/404Page';
-import UserProfile from './UserProfile';
+import UserInfoContainer from './UserInfoContainer';
 
 const UserInfo = () => {
   const { username = '' } = useParams();
-
+  const { data: myInfo } = useMyInfo();
   const { data: userList = [] } = useGetUsersList({});
-  const isUserExist = isValueUniqueInArray(userList, 'username', username);
 
-  if (isUserExist === false) {
+  const isUserExist = isValueUniqueInArray(userList, 'username', username);
+  const isSameUser = username === myInfo?.username;
+
+  if (isUserExist === false && isSameUser === false) {
     return <ErrorPage />;
+  }
+
+  if (!myInfo) {
+    return null;
   }
 
   return (
     <Box border="1px solid" height="100vh">
       <PageHeader pageName={username} />
-      <Box padding="25px 20px">
-        <UserProfile userList={userList} username={username} />
-        <Box mt="55px">
-          <Heading></Heading>
-          <Box>잔디 자리</Box>
-        </Box>
-      </Box>
+      <UserInfoContainer
+        userList={userList}
+        myInfo={myInfo}
+        isSameUser={isSameUser}
+        username={username}
+      />
     </Box>
   );
 };
