372be98d91262113f25026525cd5d925c02fe64c
이종혁 <whdgur57117@gmail.com>
2024-01-15 03:04:24 +0900

    ✨ ReflectionViewPage 컴포넌트 추가 및 storybook 생성

---
+++ src/pages/ReflectionViewPage/index.tsx
@@ -0,0 +1,13 @@
+import PageHeader from '@/components/PageHeader';
+import ReflectionDetail from './ReflectionDetail';
+
+const ReflectionViewPage = () => {
+  return (
+    <>
+      <PageHeader pageName="회고"></PageHeader>
+      <ReflectionDetail />
+    </>
+  );
+};
+
+export default ReflectionViewPage;
