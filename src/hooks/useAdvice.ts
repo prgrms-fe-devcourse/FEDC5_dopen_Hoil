42a3435dce0917964600912a64e096e8c710d390
JaeHyunGround <gothddlek@naver.com>
2024-01-14 15:22:55 +0900

    🎨 useAdvice isLoading를 활용하여 로딩일 때 경우 처리

--- src/hooks/useAdvice.ts
+++ src/hooks/useAdvice.ts
@@ -2,19 +2,11 @@ import axios from 'axios';
 import { useQuery } from 'react-query';
 
 export const useAdvice = () => {
-  const { data, isError } = useQuery(
-    'advice',
-    async () => {
-      return await axios
-        .get('https://api.adviceslip.com/advice')
-        .then((res) => res.data.slip.advice);
-    },
-    {
-      meta: {
-        errorMessage: '명언을 불러올 때 에러가 발생했습니다.',
-      },
-    },
-  );
+  const { data, isError, isLoading } = useQuery('advice', async () => {
+    return await axios
+      .get('https://api.adviceslip.com/advice')
+      .then((res) => res.data.slip.advice);
+  });
 
-  return { data, isError };
+  return { data, isError, isLoading };
 };
