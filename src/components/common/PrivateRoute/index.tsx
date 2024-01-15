0d4b144ade07f737241266084e55d47538c46e02
loevray <rei980213@gmail.com>
2024-01-15 20:31:22 +0900

    :bug: href 삭제 및 인증 캐싱타임0으로 변경

--- src/components/common/PrivateRoute/index.tsx
+++ src/components/common/PrivateRoute/index.tsx
@@ -1,12 +1,11 @@
 import { useCheckUserAuth } from '@/hooks/useAuth';
 
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet } from 'react-router-dom';
 
 export default function PrivateRoute() {
   const result = useCheckUserAuth();
   if (result.isSuccess && result.data) {
     return <Outlet context={result} key="test" />;
   }
-  location.href = '/login';
-  return <></>;
+  return <Navigate to="/login" />;
 }
