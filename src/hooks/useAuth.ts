479127b1c0ae913699e47b1e825e4203522bf73d
loevray <rei980213@gmail.com>
2024-01-15 13:54:08 +0900

    :bug: main.tsx에서 받아오던 에러처리 조건문 수정 및 버그 수정, useCheckUserAuth 수정 - 메인에러처리 : 401,400에러일땐 아무것도 안함 - useCheckUserAuth에서 조건부로 에러 바운더리 사용 및 에러 발생시 전역으로 전달할 meta필드 에러메시지 작성

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -156,7 +156,15 @@ export const useMyInfo = ({ onSuccessFn }: AuthProps = {}) => {
 export const useCheckUserAuth = () => {
   return useQuery(AUTH, checkUserAuthentication, {
     suspense: true,
-    useErrorBoundary: true,
+    useErrorBoundary: ({ query }) => {
+      if (!query) {
+        return false;
+      }
+      return true;
+    },
     retry: 0,
+    meta: {
+      errorMessage: '로그인이 필요한 페이지입니다',
+    },
   });
 };
