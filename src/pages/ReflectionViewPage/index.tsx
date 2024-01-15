046b3583a262f668786fd20aac8d8123bf72872e
이종혁 <whdgur57117@gmail.com>
2024-01-15 23:54:52 +0900

    ✨ ReflectionViewPage Errorboundary,suspense 적용

--- src/pages/ReflectionViewPage/index.tsx
+++ src/pages/ReflectionViewPage/index.tsx
@@ -1,11 +1,21 @@
 import PageHeader from '@/components/PageHeader';
 import ReflectionDetail from './ReflectionDetail';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Spinner } from '@chakra-ui/react';
+import { Suspense } from 'react';
+import ErrorFallback from '../PostViewPage/ErrorFallback';
+import { useQueryErrorResetBoundary } from 'react-query';
 
 const ReflectionViewPage = () => {
+  const { reset } = useQueryErrorResetBoundary();
   return (
     <>
       <PageHeader pageName="회고"></PageHeader>
-      <ReflectionDetail />
+      <ErrorBoundary onReset={reset} FallbackComponent={ErrorFallback}>
+        <Suspense fallback={<Spinner />}>
+          <ReflectionDetail />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
