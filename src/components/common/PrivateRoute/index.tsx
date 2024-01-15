65e654908ad298e15f15db2a5f5217637df98bb9
loevray <rei980213@gmail.com>
2024-01-15 19:11:31 +0900

    :bug: 인증필요한 페이지 Navigate 컴포넌트로 리다이렉트 하는 대신 location.href로 처리 - 리렌더링 강제 발생하게 하였습니다..

--- src/components/common/PrivateRoute/index.tsx
+++ src/components/common/PrivateRoute/index.tsx
@@ -1,12 +1,12 @@
 import { useCheckUserAuth } from '@/hooks/useAuth';
 
-import { Navigate, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 export default function PrivateRoute() {
   const result = useCheckUserAuth();
   if (result.isSuccess && result.data) {
-    return <Outlet context={result} />;
+    return <Outlet context={result} key="test" />;
   }
-
-  return <Navigate replace to="/login" />;
+  location.href = '/login';
+  return <></>;
 }
