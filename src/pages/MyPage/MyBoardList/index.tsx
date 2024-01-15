90228bad62a3aaf0f8b5fa2b28ec18d46c46361a
seunghyun <tmdgus7820@naver.com>
2024-01-15 17:08:42 +0900

    ⚡️ MyBoardList 페이지 게시글 작성 시간 및 타이머 게시글을 제외한 게시글만 나타나도록 수정

--- src/pages/MyPage/MyBoardList/index.tsx
+++ src/pages/MyPage/MyBoardList/index.tsx
@@ -1,23 +1,25 @@
 import { useNavigate } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 
+import { useMyInfo } from '@/hooks/useAuth';
 import { useMyPostList } from '@/hooks/usePost';
 
 import PostListItem from '@/components/PostList/PostListItem';
 import PageHeader from '@/components/PageHeader';
 import Footer from '@/components/Footer';
+import { calculateTimeDiff } from '@/utils/calculateTimeDiff';
 
 const MyBoardList = () => {
   const navigate = useNavigate();
-  const { data: myPostList = [], isLoading, isError } = useMyPostList();
+  const { data: myInfo } = useMyInfo();
+  const { data: postList = [] } = useMyPostList();
 
-  if (isLoading) {
-    return <Box>로딩중입니다...</Box>;
+  if (!myInfo) {
+    return;
   }
 
-  if (isError) {
-    return <Box>새로고침 해주세요...</Box>;
-  }
+  const { timerChannelId } = JSON.parse(myInfo.fullName);
+  const myPostList = postList.filter((post) => post.channel !== timerChannelId);
 
   const onPostDetail = (id: string) => {
     navigate(`./${id}`);
@@ -27,11 +29,16 @@ const MyBoardList = () => {
     <Box>
       <PageHeader pageName="내가 작성한 게시글" />
       <Box padding="0 0 20px">
+        {myPostList.length === 0 && (
+          <Box textAlign="center" fontSize="14px" p="50px 0">
+            작성한 게시글이 없습니다.
+          </Box>
+        )}
         {myPostList.map((post) => (
           <PostListItem
             key={post._id}
             title={post.title}
-            timeAgo="2일 전"
+            timeAgo={calculateTimeDiff(post.createdAt) || ''}
             username=""
             likeCount={post.likes.length}
             commentCount={post.comments.length}
