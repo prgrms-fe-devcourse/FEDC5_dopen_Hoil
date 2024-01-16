46c7090fbefe257e06bee127f27de261d198cf03
JaeHyunGround <gothddlek@naver.com>
2024-01-16 13:02:31 +0900

    🐛 SearchPage 검색 시 요청이 여러번 들어가는 버그 수정:

--- src/pages/SearchPage/OptionSelector.tsx
+++ src/pages/SearchPage/OptionSelector.tsx
@@ -1,32 +1,43 @@
 import { Channel } from '@/apis/type';
 import { DEFAULT_PAGE_PADDING } from '@/constants/style';
-import { Box, Select, SelectProps } from '@chakra-ui/react';
+import {
+  Box,
+  FormControl,
+  FormErrorMessage,
+  Select,
+  SelectProps,
+} from '@chakra-ui/react';
 import { Dispatch, SetStateAction } from 'react';
 
 interface SearchOptionSelectorProps extends SelectProps {
+  option: string;
   setOption: Dispatch<SetStateAction<string>>;
   channelListData?: Channel[];
 }
 
 const OptionSelector = ({
+  option = '',
   setOption,
   channelListData = [],
   ...props
 }: SearchOptionSelectorProps) => {
   return (
     <Box p={`0 ${DEFAULT_PAGE_PADDING}`}>
-      <Select
-        placeholder="검색 조건"
-        onChange={(e) => setOption(e.target.value)}
-        {...props}
-      >
-        <option value="유저">유저</option>
-        {channelListData?.map((option) => (
-          <option value={option.name} key={option._id}>
-            {option.description}
-          </option>
-        ))}
-      </Select>
+      <FormControl isInvalid={!option}>
+        <Select
+          placeholder="검색 조건"
+          onChange={(e) => setOption(e.target.value)}
+          {...props}
+        >
+          <option value="유저">유저</option>
+          {channelListData?.map((option) => (
+            <option value={option.name} key={option._id}>
+              {option.description}
+            </option>
+          ))}
+        </Select>
+        {!option && <FormErrorMessage>옵션을 선택해주세요.</FormErrorMessage>}
+      </FormControl>
     </Box>
   );
 };
