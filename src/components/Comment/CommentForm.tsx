7093b68e67f6414ab409e813ef74fedbe9f89ac0
seunghyun <tmdgus7820@naver.com>
2024-01-16 14:51:31 +0900

    ⚡️ Comment에 대한 예외 처리 추가

--- src/components/Comment/CommentForm.tsx
+++ src/components/Comment/CommentForm.tsx
@@ -1,5 +1,12 @@
 import styled from '@emotion/styled';
-import { Avatar, FormLabel, Textarea, Image } from '@chakra-ui/react';
+import {
+  Avatar,
+  FormLabel,
+  Textarea,
+  Image,
+  Text,
+  Box,
+} from '@chakra-ui/react';
 import { useCreateComment } from '@/hooks/useComment';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useEffect } from 'react';
@@ -14,11 +21,25 @@ export interface CommentInput {
 }
 
 const CommentForm = ({ id, image }: CommentFormProps) => {
-  const { register, handleSubmit, reset } = useForm<CommentInput>();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    setError,
+    formState: { errors },
+  } = useForm<CommentInput>();
 
   const { pushComment, isSuccess } = useCreateComment();
 
   const onCommentValid: SubmitHandler<CommentInput> = ({ comment }) => {
+    if (comment.trim().length < 1) {
+      setError(
+        'comment',
+        { message: '댓글을 작성해주세요.' },
+        { shouldFocus: true },
+      );
+      return;
+    }
     pushComment({ comment, postId: id });
     reset();
   };
@@ -33,25 +54,35 @@ const CommentForm = ({ id, image }: CommentFormProps) => {
   }, [isSuccess]);
 
   return (
-    <Form onSubmit={handleSubmit(onCommentValid)}>
-      <FormLabel htmlFor="comment" m="0">
-        <Avatar w="40px" h="40px" src={image} />
-      </FormLabel>
-      <Textarea
-        resize="none"
-        bgColor="gray.200"
-        w="calc(100% - 84px)"
-        h="40px"
-        borderRadius="5px"
-        margin="0 10px"
-        placeholder="댓글을 입력해주세요."
-        id="comment"
-        {...register('comment', {})}
-      />
-      <Button>
-        <Image src="/src/assets/send.svg" alt="comment send" />
-      </Button>
-    </Form>
+    <Box>
+      <Form onSubmit={handleSubmit(onCommentValid)}>
+        <FormLabel htmlFor="comment" m="0">
+          <Avatar w="40px" h="40px" src={image} />
+        </FormLabel>
+        <Textarea
+          resize="none"
+          bgColor="gray.200"
+          w="calc(100% - 84px)"
+          h="40px"
+          borderRadius="5px"
+          margin="0 10px"
+          placeholder="댓글을 입력해주세요."
+          id="comment"
+          {...register('comment', {
+            maxLength: {
+              value: 100,
+              message: '댓글을 100자 이하로 작성해주세요.',
+            },
+          })}
+        />
+        <Button>
+          <Image src="/src/assets/send.svg" alt="comment send" />
+        </Button>
+      </Form>
+      <Text m="5px 0 0 55px" fontSize="1.2rem" color="pink.300">
+        {errors && errors['comment'] && errors['comment']?.message}
+      </Text>
+    </Box>
   );
 };
 
