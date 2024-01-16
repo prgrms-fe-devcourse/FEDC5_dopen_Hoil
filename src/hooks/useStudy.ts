15e57de0efd0a0c9312088604e60e7f280465cf1
seunghyun <tmdgus7820@naver.com>
2024-01-16 17:34:10 +0900

    🐛 UseStudy hook (return문 빠진 거 급하게 수정)

--- src/hooks/useStudy.ts
+++ src/hooks/useStudy.ts
@@ -11,7 +11,7 @@ export const useStudyPost = ({ channelId }: StudyProps) => {
     if (!channelId) {
       return [];
     }
-    await getPostListByChannel({ channelId });
+    return await getPostListByChannel({ channelId });
   });
 
   return {
