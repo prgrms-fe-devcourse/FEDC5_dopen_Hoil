3b32686534b57b239abd2d1e94c48c3c1649b8ef
seunghyun <tmdgus7820@naver.com>
2024-01-16 18:17:55 +0900

    💄 MyCommentList 페이지 content 영역 고정 값 갖도록 Style 수정(스크롤 바 커스텀)

--- src/pages/MyPage/MyCommentList/index.tsx
+++ src/pages/MyPage/MyCommentList/index.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled';
 import { Box, UnorderedList } from '@chakra-ui/react';
 import { useMyComment } from '@/hooks/useMyComment';
 import { useMyInfo } from '@/hooks/useAuth';
@@ -18,27 +19,49 @@ const MyCommentList = () => {
   return (
     <Box>
       <PageHeader pageName="내가 작성한 댓글" />
-      <UnorderedList listStyleType="none" ml="0" p="20px">
-        {myCommentList.length === 0 && (
-          <Box textAlign="center" fontSize="14px" p="50px 0">
-            작성한 댓글이 없습니다.
-          </Box>
-        )}
-        {myCommentList.map(({ _id, comment, createdAt }) => (
-          <MyCommentListItem
-            key={_id}
-            id={_id}
-            comment={comment}
-            image={image}
-            username={username}
-            lineClamp={3}
-            createdAt={createdAt}
-          />
-        ))}
-      </UnorderedList>
+      <CommentListContainer>
+        <UnorderedList listStyleType="none" ml="0" p="20px">
+          {myCommentList.length === 0 && (
+            <Box textAlign="center" fontSize="14px" p="50px 0">
+              작성한 댓글이 없습니다.
+            </Box>
+          )}
+          {myCommentList.map(({ _id, comment, createdAt }) => (
+            <MyCommentListItem
+              key={_id}
+              id={_id}
+              comment={comment}
+              image={image}
+              username={username}
+              lineClamp={3}
+              createdAt={createdAt}
+            />
+          ))}
+        </UnorderedList>
+      </CommentListContainer>
       <Footer />
     </Box>
   );
 };
+
+const CommentListContainer = styled.div`
+  overflow-y: auto;
+  height: calc(100vh - 160px);
+
+  &::-webkit-scrollbar {
+    width: 8px; /* 스크롤바의 너비 */
+  }
+
+  &::-webkit-scrollbar-thumb {
+    height: 30%; /* 스크롤바의 길이 */
+    background: #e2e2e2; /* 스크롤바의 색상 */
+
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #fcfcfc; /*스크롤바 뒷 배경 색상*/
+  }
+`;
 
 export default MyCommentList;
