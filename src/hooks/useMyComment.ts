ed3af78e64bd101283630f5e2a76337787973240
이종혁 <whdgur57117@gmail.com>
2024-01-16 12:15:52 +0900

    🐛 타입명 수정

--- src/hooks/useMyComment.ts
+++ src/hooks/useMyComment.ts
@@ -1,17 +1,17 @@
 import { useQuery } from 'react-query';
 import { checkAuthenticated } from '@/apis/authentication';
 import { getUserInfo } from '@/apis/userInfo';
-import { Comment } from '@/apis/type';
+import { TComment } from '@/apis/type';
 import { MY_COMMENT_LIST } from '@/constants/queryKeys';
 
 export const useMyComment = () => {
-  return useQuery<Comment[]>(MY_COMMENT_LIST, async () => {
+  return useQuery<TComment[]>(MY_COMMENT_LIST, async () => {
     const { _id } = await checkAuthenticated();
     const userInfo = await getUserInfo(_id);
 
     const isCommentArray = (
-      array: string[] | Comment[],
-    ): array is Comment[] => {
+      array: string[] | TComment[],
+    ): array is TComment[] => {
       return array.length === 0 || typeof array[0] !== 'string';
     };
 
