54336c953174e8c58cea27a14ca088791fba39fe
이종혁 <whdgur57117@gmail.com>
2024-01-16 19:28:27 +0900

    ✨ 메시지 없을 시 렌더링할 태그 추가

--- src/hooks/useMessageList.ts
+++ src/hooks/useMessageList.ts
@@ -18,17 +18,19 @@ export const useMessageList = () => {
     },
   );
 
-  const userDataList = data?.map(({ createdAt, message, sender, receiver }) => {
-    const otherType = sender._id === myInfo?._id ? receiver : sender;
+  const messageLogList = data?.map(
+    ({ createdAt, message, sender, receiver }) => {
+      const otherType = sender._id === myInfo?._id ? receiver : sender;
 
-    return {
-      key: createdAt,
-      userImage: otherType.coverImage,
-      username: otherType.username,
-      content: message,
-      subContent: calculateTimeDiff(createdAt) || '',
-      userId: otherType._id,
-    };
-  });
-  return { userDataList };
+      return {
+        key: createdAt,
+        userImage: otherType.coverImage,
+        username: otherType.username,
+        content: message,
+        subContent: calculateTimeDiff(createdAt) || '',
+        userId: otherType._id,
+      };
+    },
+  );
+  return messageLogList ? messageLogList : [];
 };
