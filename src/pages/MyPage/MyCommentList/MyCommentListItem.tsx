48cb0a831f4c7de2a331a142367b20db87bcc4c1
seunghyun <tmdgus7820@naver.com>
2024-01-17 01:29:09 +0900

    ✨ MyCommentList(내가 작성한 댓글)에서 댓글 누르면 해당 게시글로 이동

--- src/pages/MyPage/MyCommentList/MyCommentListItem.tsx
+++ src/pages/MyPage/MyCommentList/MyCommentListItem.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ListItem, Box, Text } from '@chakra-ui/react';
 import { useDeleteComment } from '@/hooks/useComment';
+import { usePostDetail } from '@/hooks/usePost';
+import { calculateTimeDiff } from '@/utils/calculateTimeDiff';
 import UserContentBlock from '@/components/common/UserContentBlock';
 import Confirm from '@/components/common/Confirm';
-import { calculateTimeDiff } from '@/utils/calculateTimeDiff';
+
 interface MyCommentListItemProps {
   id: string;
   image: string;
+  postId: string;
   username: string;
   comment: string;
   createdAt: string;
@@ -16,12 +20,14 @@ interface MyCommentListItemProps {
 const MyCommentListItem = ({
   id,
   image,
+  postId,
   username,
   comment,
   createdAt,
   lineClamp = 1,
 }: MyCommentListItemProps) => {
   const [isConfirm, setIsConfirm] = useState(false);
+  const navigate = useNavigate();
   const deleteCommentMutate = useDeleteComment();
 
   const onConfirm = () => {
@@ -33,21 +39,41 @@ const MyCommentListItem = ({
     setIsConfirm(false);
   };
 
+  const { data: myPost } = usePostDetail({ id: postId });
+
+  const onMoveMyComment = (id: string, channel: string) => {
+    switch (channel) {
+      case 'free':
+        navigate(`/board/free/${id}`);
+        break;
+      case 'reflection':
+        navigate(`/board/reflection/${id}`);
+        break;
+      case 'infoshare':
+        navigate(`/board/infoshare/${id}`);
+        break;
+    }
+  };
+
   return (
     <ListItem key={id} p="10px 0 10px" borderBottom="1px solid #D9D9D9">
       <UserContentBlock
         p="0"
         w="100%"
-        cursor="default"
         userImage={image}
         userImageSize="40px"
         username={username}
         isOnline={false}
         content={calculateTimeDiff(createdAt) || ''}
         subContent="삭제"
+        onContentClick={() => onMoveMyComment(postId, myPost.channel?.name)}
         onSubContentClick={() => setIsConfirm(true)}
       />
-      <Box mt="10px">
+      <Box
+        pt="10px"
+        cursor="pointer"
+        onClick={() => onMoveMyComment(postId, myPost.channel?.name)}
+      >
         <Text noOfLines={lineClamp} fontSize="1.3rem">
           {comment}
         </Text>
