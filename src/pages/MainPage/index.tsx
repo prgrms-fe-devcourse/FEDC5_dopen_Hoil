dbb5ebd4b8946ee45ba447649fb0adfefe91a96b
JaeHyunGround <gothddlek@naver.com>
2024-01-16 14:47:36 +0900

    🐛 Grass 데이터 가공

--- src/pages/MainPage/index.tsx
+++ src/pages/MainPage/index.tsx
@@ -1,4 +1,4 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, Text } from '@chakra-ui/react';
 import styled from '@emotion/styled';
 import { DEFAULT_PAGE_PADDING } from '@/constants/style';
 import { useMyInfo } from '@/hooks/useAuth';
@@ -8,14 +8,39 @@ import GuestProfile from '@/pages/MainPage/GuestProfile';
 import LoginProfile from '@/pages/MainPage/LoginProfile';
 import Dday from '@/pages/MainPage/Dday';
 import BoardListPreview from '@/pages/MainPage/BoardListPreview';
-import GrassBox from '@/pages/MainPage/GrassBox';
+import { useStudyPost } from '@/hooks/useStudy';
+import Grass from '@/components/Grass';
 
 const MainPage = () => {
   const { data: myInfo } = useMyInfo();
 
-  if (!myInfo) {
-    return null;
-  }
+  const timerChannelId = myInfo && JSON.parse(myInfo.fullName).timerChannelId;
+  const { studyPost = [] } = useStudyPost({
+    channelId: timerChannelId,
+  });
+
+  const studyPosts = (studyPost.length &&
+    studyPost.map(({ title, createdAt }) => ({
+      time: title,
+      createdAt,
+    }))) || [
+    {
+      time: '01:00:00',
+      createdAt: '2024-01-12T07:58:20.808Z',
+    },
+    {
+      time: '00:30:00',
+      createdAt: '2024-01-15T07:58:20.808Z',
+    },
+    {
+      time: '10:00:00',
+      createdAt: '2024-01-27T07:58:20.808Z',
+    },
+    {
+      time: '00:00:30',
+      createdAt: '2024-01-01T07:58:20.808Z',
+    },
+  ];
 
   return (
     <>
@@ -30,7 +55,12 @@ const MainPage = () => {
         <MainPageBody>
           {myInfo ? <LoginProfile myInfo={myInfo} /> : <GuestProfile />}
           <Dday myInfo={myInfo} />
-          <GrassBox myInfo={myInfo} />
+          {!myInfo && (
+            <Text fontSize="1.5rem" fontWeight="bold" color="black" mb="17px">
+              여러분의 열정을 기록해 보세요.
+            </Text>
+          )}
+          <Grass timerPosts={studyPosts} />
           <BoardListPreview />
         </MainPageBody>
         <Footer position="sticky" bottom="0" />
