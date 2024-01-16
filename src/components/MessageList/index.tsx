0011d2b62d3cb67842d334356e766c51b2cb4c28
이종혁 <whdgur57117@gmail.com>
2024-01-16 19:28:02 +0900

    ♻️ 변수명 변경

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,5 +1,5 @@
 import { useNavigate } from 'react-router-dom';
-import { Flex, FlexProps } from '@chakra-ui/react';
+import { Box, Flex, FlexProps } from '@chakra-ui/react';
 import { useMessageList } from '@/hooks/useMessageList';
 import UserContentBlock from '../common/UserContentBlock';
 
@@ -7,24 +7,28 @@ interface MessageListProps extends FlexProps {}
 
 const MessageList = ({ ...props }: MessageListProps) => {
   const navigate = useNavigate();
-  const { userDataList } = useMessageList();
+  const messageLogList = useMessageList();
 
   return (
     <Flex flexDir="column" overflowY="auto" {...props}>
-      {userDataList?.map(
-        ({ key, userImage, username, content, subContent, userId }) => {
-          return (
-            <UserContentBlock
-              key={key}
-              userImage={userImage}
-              username={username}
-              content={content}
-              subContent={subContent}
-              onClick={() => navigate(`./${userId}`)}
-              ellipsis={2}
-            ></UserContentBlock>
-          );
-        },
+      {messageLogList.length ? (
+        messageLogList.map(
+          ({ key, userImage, username, content, subContent, userId }) => {
+            return (
+              <UserContentBlock
+                key={key}
+                userImage={userImage}
+                username={username}
+                content={content}
+                subContent={subContent}
+                onClick={() => navigate(`./${userId}`)}
+                ellipsis={2}
+              ></UserContentBlock>
+            );
+          },
+        )
+      ) : (
+        <Box>메시지가 없습니다</Box>
       )}
     </Flex>
   );
