6cef6bc29e25b4b2b3e5fb47c1a12e8b133a35cb
seunghyun <tmdgus7820@naver.com>
2024-01-17 12:56:31 +0900

    ⚡️ 검색에 대한 예외 처리 수정

--- src/pages/SearchPage/SearchInput.tsx
+++ src/pages/SearchPage/SearchInput.tsx
@@ -14,20 +14,24 @@ import { DEFAULT_PAGE_PADDING } from '@/constants/style';
 // TODO : Input 컴포넌트인데 BoxProps를 받아오니깐 어색... 새로운 작명이 필요
 interface SearchInputProps extends BoxProps {
   keyword: string;
+  isSearch: boolean;
   disabled?: boolean;
+  inputRef: React.MutableRefObject<HTMLInputElement | null>;
   setKeyword: Dispatch<SetStateAction<string>>;
   onSearchSubmit: (e: FormEvent<HTMLFormElement>) => void;
 }
 
 const SearchInput = ({
   keyword = '',
+  isSearch = false,
   disabled,
+  inputRef,
   setKeyword,
   onSearchSubmit,
   ...props
 }: SearchInputProps) => {
   return (
-    <FormControl isInvalid={!keyword}>
+    <FormControl isInvalid={!keyword.trim().length && isSearch}>
       <Box p={`0 ${DEFAULT_PAGE_PADDING}`} {...props}>
         <Flex
           padding="8px 16px"
@@ -44,13 +48,12 @@ const SearchInput = ({
               onChange={(e) => setKeyword(e.target.value)}
               placeholder="검색어를 입력하세요."
               focusBorderColor="transparent"
+              ref={inputRef}
             />
           </form>
           <CloseButton ml="5px" onClick={() => setKeyword('')} />
         </Flex>
-        {!keyword && (
-          <FormErrorMessage>검색어를 입력해주세요.</FormErrorMessage>
-        )}
+        <FormErrorMessage>검색어를 입력해주세요.</FormErrorMessage>
       </Box>
     </FormControl>
   );
