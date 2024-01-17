30394f0f571122e0fe100310ab156a4c5b60a69e
seunghyun <tmdgus7820@naver.com>
2024-01-17 23:02:47 +0900

    💄 Error 페이지 간단한 Style 처리

--- src/pages/PostViewPage/ErrorFallback.tsx
+++ src/pages/PostViewPage/ErrorFallback.tsx
@@ -12,9 +12,19 @@ const ErrorFallback = ({ error, resetErrorBoundary }: ErrorFallbackProps) => {
   const navigate = useNavigate();
   if (error?.response?.status === 401) {
     return (
-      <Box>
-        <Box>권한이 없습니다</Box>
-        <Button onClick={() => navigate('/login')}>
+      <Box
+        width="100vw"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+        textAlign="center"
+        p="200px 0"
+      >
+        <Box width="100%" fontSize="2rem" mb="20px">
+          권한이 없습니다
+        </Box>
+        <Button ml="20px" onClick={() => navigate('/login')}>
           로그인 화면으로 이동하기
         </Button>
       </Box>
@@ -23,10 +33,22 @@ const ErrorFallback = ({ error, resetErrorBoundary }: ErrorFallbackProps) => {
 
   if (!error?.response && !(error instanceof AuthError)) {
     return (
-      <Box>
-        <Box>에러가 발생했습니다</Box>
+      <Box
+        width="100vw"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+        textAlign="center"
+        p="200px 0"
+      >
+        <Box width="100%" fontSize="2rem" mb="20px">
+          에러가 발생했습니다
+        </Box>
         <Button onClick={() => resetErrorBoundary()}>다시 시도하기</Button>
-        <Button onClick={() => navigate(-1)}>뒤로가기</Button>
+        <Button ml="20px" onClick={() => navigate(-1)}>
+          뒤로가기
+        </Button>
       </Box>
     );
   }
