83e257104a78dbe249661fbc1edf2859039ac37b
seunghyun <tmdgus7820@naver.com>
2024-01-20 10:19:28 +0900

    ✨ Follower, Following 페이지 연결

--- src/pages/UserInfo/UserInfoContainer.tsx
+++ src/pages/UserInfo/UserInfoContainer.tsx
@@ -1,8 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 import { User } from '@/apis/type';
 import UserProfile from './UserProfile';
 import UserFollowInfo from './UserFollowInfo';
 import UserGrass from './UserGrass';
+import Following from './Following';
+import Follower from './Follower';
+import { useEffect, useState } from 'react';
 
 interface UserInfoContainerProps {
   userList: User[];
@@ -17,18 +21,69 @@ const UserInfoContainer = ({
   isSameUser,
   username,
 }: UserInfoContainerProps) => {
-  const userInfo = userList.filter((user) => user.username === username)[0];
+  const [isFollowing, setIsFollowing] = useState(false);
+  const userInfo =
+    userList.filter((user) => user.username === username)[0] ?? myInfo;
+
+  const [followId, setFollowId] = useState('');
+
+  const { image, _id: userId } = userInfo;
+  const { following } = myInfo;
+
+  useEffect(() => {
+    const isAlreadyFollowing = following.some(
+      ({ user: followingUserId, _id }) => {
+        if (followingUserId === userId) {
+          setFollowId(_id);
+          return true;
+        }
+      },
+    );
+    onupdateFollowing(isAlreadyFollowing);
+  }, [userInfo]);
+
+  const onupdateFollowing = (isFollowing: boolean) => {
+    setIsFollowing(isFollowing);
+  };
 
   return (
     <Box padding="25px 0">
-      <UserProfile
-        userInfo={userInfo ?? myInfo}
-        myInfo={myInfo}
-        username={username}
-        isSameUser={isSameUser}
-      />
-      <UserFollowInfo followInfo={userInfo ?? myInfo} />
-      <UserGrass userInfo={userInfo ?? myInfo} />
+      <Routes>
+        <Route
+          path="follow"
+          element={
+            <Following userInfo={userInfo ?? myInfo} isSameUser={isSameUser} />
+          }
+        />
+        <Route
+          path="follower"
+          element={
+            <Follower
+              userInfo={userInfo ?? myInfo}
+              isFollowing={isFollowing}
+              isSameUser={isSameUser}
+            />
+          }
+        />
+        <Route
+          path="/*"
+          element={
+            <>
+              <UserProfile
+                image={image}
+                userId={userId}
+                followId={followId}
+                username={username}
+                isSameUser={isSameUser}
+                isFollowing={isFollowing}
+                onupdateFollowing={onupdateFollowing}
+              />
+              <UserFollowInfo followInfo={userInfo ?? myInfo} />
+              <UserGrass userInfo={userInfo ?? myInfo} />
+            </>
+          }
+        />
+      </Routes>
     </Box>
   );
 };
